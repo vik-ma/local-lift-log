f894b47767595582a734184d1bfab416f5b22e9e
Viktor <vikt.mag@gmail.com>
2024-02-27 21:38:19 +0100

    Add TestGreeting component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import TestGreeting from "./components/TestGreeting";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <TestGreeting />
       </div>
     </main>
   );
