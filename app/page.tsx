495c19a59cfcdd2a0e4c2c17122e9dd2be2b327c
Viktor <vikt.mag@gmail.com>
2024-02-27 23:56:57 +0100

    Create routines page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TestGreeting from "./components/TestGreeting";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <TestGreeting />
+        <Link href={"/routines"}>Routines</Link>
       </div>
     </main>
   );
---
+++ app/routines/page.tsx
@@ -0,0 +1,9 @@
+export default function Routines() {
+  return (
+    <>
+      <div>
+        <h1>Routines</h1>
+      </div>
+    </>
+  );
+}
