faa4820258a891aaa6d63ff81d83785a1aa6eb67
Viktor <vikt.mag@gmail.com>
2024-02-29 19:04:04 +0100

    Add Back button to SiteHeader

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,10 +1,15 @@
-import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
-import { NavLink } from "react-router-dom";
+import { Navbar, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function SiteHeader() {
+  const navigate = useNavigate();
+
   return (
     <Navbar isBordered>
       <NavbarContent className="flex gap-4">
+        <NavbarItem>
+          <Button onClick={() => navigate(-1)}>Back</Button>
+        </NavbarItem>
         <NavbarItem>
           <NavLink to="/">Home</NavLink>
         </NavbarItem>
