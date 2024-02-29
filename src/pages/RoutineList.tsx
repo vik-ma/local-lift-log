4a8a29f0d587b1feaff666b3105879db3a1a32f2
Viktor <vikt.mag@gmail.com>
2024-02-29 18:37:33 +0100

    Add Input and Button to RoutineList page

--- src/pages/RoutineList.tsx
+++ src/pages/RoutineList.tsx
@@ -1,11 +1,20 @@
-export default function RoutineListPage() {
+import { Button, Input } from "@nextui-org/react";
 
+export default function RoutineListPage() {
   return (
     <>
-      <div className="bg-neutral-900 px-6 py-4 rounded rounded-xl">
-        <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
-          Routines
-        </h1>
+      <div className="flex flex-col gap-4">
+        <div className="bg-neutral-900 px-6 py-4 rounded rounded-xl">
+          <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
+            Routines
+          </h1>
+        </div>
+        <div className="flex flex-row gap-2 items-center">
+          <Input type="text" label="Id" placeholder="Enter Routine Id" />
+          <Button className="text-lg" size="lg" color="primary">
+            OK
+          </Button>
+        </div>
       </div>
     </>
   );
