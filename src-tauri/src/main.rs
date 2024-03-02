1f7ea6a1f74f26298613f549594558660875a235
Viktor <vikt.mag@gmail.com>
2024-03-02 14:34:12 +0100

    Add tauri_plugin_sql to main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_tables",
+        sql: "CREATE TABLE routines (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          note TEXT,
+          is_schedule_weekly BOOLEAN,
+          num_days_in_schedule SMALLINT,
+          custom_schedule_start_date TEXT
+          );",
+        kind: MigrationKind::Up,
+    }];
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:local_lift_log_db.db", migrations)
+                .build(),
+        )
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
