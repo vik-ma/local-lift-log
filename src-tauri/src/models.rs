8cc373b7ebabd1570c50e15f5462d97219151544
Viktor <vikt.mag@gmail.com>
2024-03-02 13:12:15 +0100

    Create models.rs

---
+++ src-tauri/src/models.rs
@@ -0,0 +1,13 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::routines)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Routine {
+    pub id: i32,
+    pub name: String,
+    pub note: Option<String>,
+    pub is_schedule_weekly: bool,
+    pub num_days_in_schedule: i16,
+    pub custom_schedule_start_date: Option<String>,
+}
