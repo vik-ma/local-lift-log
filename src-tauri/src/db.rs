19fb1b687ff42cf28ed9cc158d7f1314b964d3d0
Viktor <vikt.mag@gmail.com>
2024-03-03 19:17:43 +0100

    Create db.rs

---
+++ src-tauri/src/db.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use std::path::Path;
+
+pub fn init() {
+    if !db_file_exists() {
+        create_db_file();
+    }
+}
+
+fn create_db_file() {
+    let db_path = get_db_path();
+    let db_dir = Path::new(&db_path).parent().unwrap();
+
+    if !db_dir.exists() {
+        fs::create_dir_all(db_dir).unwrap();
+    }
+
+    fs::File::create(db_path).unwrap();
+}
+
+fn db_file_exists() -> bool {
+    let db_path = get_db_path();
+    Path::new(&db_path).exists()
+}
+
+fn get_db_path() -> String {
+    let module_path = std::module_path!();
+    let parent_dir = Path::new(&module_path).parent().unwrap();
+    let db_path = parent_dir.join("local_lift_log_db.sqlite");
+    db_path.to_str().unwrap().to_string()
+}
