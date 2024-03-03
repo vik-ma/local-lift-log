e9baa3111152979e2c1813bfbc2828119e549865
Viktor <vikt.mag@gmail.com>
2024-03-03 20:09:04 +0100

    Add run_migrations and establish_connection functions to db.rs

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,12 +1,33 @@
 use dotenvy::dotenv;
+
 use std::env;
 use std::fs;
 use std::path::Path;
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 pub fn init() {
     if !db_file_exists() {
         create_db_file();
     }
+
+    run_migrations();
+}
+
+fn run_migrations() {
+    let mut connection = establish_connection();
+    connection.run_pending_migrations(MIGRATIONS).unwrap();
+}
+
+fn establish_connection() -> SqliteConnection {
+    let db_path = "sqlite://".to_string() + get_db_path().as_str();
+
+    SqliteConnection::establish(&db_path)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", db_path))
 }
 
 fn create_db_file() {
