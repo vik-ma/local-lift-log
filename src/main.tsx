f3976740e65adbeb04f9bfa62be55a4ac0c73ffe
Viktor <vikt.mag@gmail.com>
2024-03-05 15:16:25 +0100

    Uninstall React Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
