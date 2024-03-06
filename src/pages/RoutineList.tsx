8ce1d079aab58941fc2f7ea2f36e4b6f09ee0a23
Viktor <vikt.mag@gmail.com>
2024-03-06 03:33:15 +0100

    Add confirmation Modal to delete Routine in RoutineList

--- src/pages/RoutineList.tsx
+++ src/pages/RoutineList.tsx
@@ -1,4 +1,12 @@
-import { Button } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Routine, UserSettings } from "../typings";
@@ -8,6 +16,7 @@ import UpdateUserSettings from "../helpers/UpdateUserSettings";
 export default function RoutineListPage() {
   const navigate = useNavigate();
   const [routines, setRoutines] = useState<Routine[]>([]);
+  const [routineToDelete, setRoutineToDelete] = useState<Routine | null>(null);
 
   const { db, userSettings, setUserSettings } = useDatabaseContext();
 
@@ -55,25 +64,58 @@ export default function RoutineListPage() {
     await UpdateUserSettings({ userSettings: updatedSettings, db: db! });
   };
 
-  const handleDeleteButtonClick = async (routine: Routine) => {
-    if (routine === null) return;
+  const handleDeleteButtonClick = (routine: Routine) => {
+    setRoutineToDelete(routine);
+    onOpen();
+  };
+
+  const deleteRoutine = () => {
+    if (routineToDelete === null) return;
+    console.log("asd");
 
     try {
       if (db === null) return;
 
-      db.execute("DELETE from routines WHERE id = $1", [routine.id]);
+      db.execute("DELETE from routines WHERE id = $1", [routineToDelete.id]);
 
       const updatedRoutines: Routine[] = routines.filter(
-        (item) => item.id !== routine.id
+        (item) => item.id !== routineToDelete?.id
       );
       setRoutines(updatedRoutines);
     } catch (error) {
       console.log(error);
     }
+
+    setRoutineToDelete(null);
+    onClose();
   };
+
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
   return (
     <>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Delete Routine
+              </ModalHeader>
+              <ModalBody>
+                <p>Are you sure you want to permanently delete Routine?</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="danger" onPress={deleteRoutine}>
+                  Delete
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <div className="flex flex-col gap-4">
         <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
           <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
