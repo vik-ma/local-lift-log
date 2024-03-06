b768b9590bce5cd93e0b807058e676744bcffb08
Viktor <vikt.mag@gmail.com>
2024-03-06 01:28:19 +0100

    Add setUserSettings value to DatabaseContext

--- src/context/DatabaseContext.tsx
+++ src/context/DatabaseContext.tsx
@@ -1,13 +1,15 @@
 import Database from "tauri-plugin-sql-api";
-import { createContext } from "react";
+import { Dispatch, SetStateAction, createContext } from "react";
 import { UserSettings } from "../typings";
 
 type DatabaseContextProps = {
   db: Database | null;
   userSettings: UserSettings | null;
+  setUserSettings: Dispatch<SetStateAction<UserSettings | null>>;
 };
 
 export const DatabaseContext = createContext<DatabaseContextProps>({
   db: null,
   userSettings: null,
+  setUserSettings: () => {},
 });
--- src/context/DatabaseContextProvider.tsx
+++ src/context/DatabaseContextProvider.tsx
@@ -78,6 +78,7 @@ export const DatabaseContextProvider = ({
       value={{
         db,
         userSettings,
+        setUserSettings,
       }}
     >
       {children}
