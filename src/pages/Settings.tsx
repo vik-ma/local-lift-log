25a90467ddcc75b74dc220eedbc5ae6ae205566a
Viktor <vikt.mag@gmail.com>
2024-03-09 20:16:49 +0100

    Add Switch to toggle show_timestamp_on_completed_set in Settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,11 +1,52 @@
+import { useState, useEffect } from "react";
+import { UserSettings } from "../typings";
+import { GetUserSettings } from "../helpers/UserSettings/GetUserSettings";
+import { Switch } from "@nextui-org/react";
+import { UpdateUserSettings } from "../helpers/UserSettings/UpdateUserSettings";
+
 export default function SettingsPage() {
+  const [userSettings, setUserSettings] = useState<UserSettings>();
+
+  useEffect(() => {
+    const loadUserSettings = async () => {
+      const settings: UserSettings | undefined = await GetUserSettings();
+      if (settings !== undefined) setUserSettings(settings);
+    };
+
+    loadUserSettings();
+  }, []);
+
+  const handleSetShowTimestampChange = async (value: boolean) => {
+    const updatedSettings: UserSettings = {
+      ...userSettings!,
+      show_timestamp_on_completed_set: value,
+    };
+
+    await updateUserSettings(updatedSettings);
+  };
+
+  const updateUserSettings = async (userSettings: UserSettings) => {
+    await UpdateUserSettings(userSettings);
+    setUserSettings(userSettings);
+  };
+
   return (
     <>
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center gap-4">
         <div className="bg-neutral-900 px-6 py-4 rounded-xl">
           <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
             Settings
           </h1>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Switch
+            color="success"
+            size="lg"
+            isSelected={userSettings?.show_timestamp_on_completed_set}
+            onValueChange={(value) => handleSetShowTimestampChange(value)}
+          >
+            Show Timestamp On Completed Sets
+          </Switch>
         </div>
       </div>
     </>
