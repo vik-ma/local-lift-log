be1077e1a4435b92fe23894e94168521e6356f0e
Viktor <vikt.mag@gmail.com>
2024-03-09 20:00:44 +0100

    Uninstall Redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./state/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/state/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSettingsReducer from "./user_settings/userSettingsSlice";
-
-export const store = configureStore({ reducer: {
-    userSettings: userSettingsReducer,
-}});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
