a5cd7526eef079076c91252dcff3605114f230f7
Viktor <vikt.mag@gmail.com>
2024-03-16 09:16:15 +0100

    Add setUnitsModal to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import { Checkbox, Button } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 import Database from "tauri-plugin-sql-api";
 import { useNavigate } from "react-router-dom";
 import { UserSettings } from "../typings";
@@ -13,6 +21,22 @@ export default function HomePage() {
   const navigate = useNavigate();
 
   const initialized = useRef(false);
+
+  const setUnitsModal = useDisclosure();
+
+  const createDefaultUserSettings = async (isMetricUnits: boolean) => {
+    const defaultUserSettings: UserSettings | undefined =
+      await CreateDefaultUserSettings();
+
+    // Create Default User Settings
+    if (defaultUserSettings !== undefined) {
+      await UpdateAllUserSettings(defaultUserSettings);
+      setUserSettings(defaultUserSettings);
+    }
+
+    // Create Default Exercise List
+    await CreateDefaultExerciseList();
+  };
 
   useEffect(() => {
     const loadUserSettings = async () => {
@@ -30,17 +54,7 @@ export default function HomePage() {
             initialized.current = true;
           } else return;
 
-          const defaultUserSettings: UserSettings | undefined =
-            await CreateDefaultUserSettings();
-
-          // Create Default User Settings
-          if (defaultUserSettings !== undefined) {
-            await UpdateAllUserSettings(defaultUserSettings);
-            setUserSettings(defaultUserSettings);
-          }
-
-          // Create Default Exercise List
-          await CreateDefaultExerciseList();
+          setUnitsModal.onOpen();
         }
       } catch (error) {
         console.log(error);
@@ -48,7 +62,7 @@ export default function HomePage() {
     };
 
     loadUserSettings();
-  }, []);
+  }, [setUnitsModal]);
 
   const addRoutine = async () => {
     const db = await Database.load(import.meta.env.VITE_DB);
@@ -76,41 +90,77 @@ export default function HomePage() {
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
-        <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
-          Home
-        </h1>
+    <>
+      <Modal
+        isOpen={setUnitsModal.isOpen}
+        onOpenChange={setUnitsModal.onOpenChange}
+        isDismissable={false}
+        hideCloseButton={true}
+      >
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Choose Units
+              </ModalHeader>
+              <ModalBody>
+                <p>Do you want to use Metric or Imperial units?</p>
+              </ModalBody>
+              <ModalFooter className="flex justify-center gap-5">
+                <Button
+                  className="text-lg font-medium"
+                  size="lg"
+                  color="primary"
+                >
+                  Metric
+                </Button>
+                <Button
+                  className="text-lg font-medium"
+                  size="lg"
+                  color="primary"
+                >
+                  Imperial
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
+          <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
+            Home
+          </h1>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-2">
+          <Button
+            className="text-lg"
+            size="lg"
+            color="primary"
+            onPress={addRoutine}
+          >
+            Add Routine
+          </Button>
+          <Button
+            className="text-lg"
+            size="lg"
+            color="primary"
+            onPress={() => navigate(`/asd`)}
+          >
+            Test Not Found
+          </Button>
+          <Button
+            className="text-lg"
+            size="lg"
+            color="primary"
+            onPress={testFunction}
+          >
+            Update Settings
+          </Button>
+          <p>Settings Id: {userSettings?.id}</p>
+          <p>Active Routine Id: {userSettings?.active_routine_id}</p>
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center gap-2">
-        <Checkbox defaultSelected>Option</Checkbox>
-        <Button
-          className="text-lg"
-          size="lg"
-          color="primary"
-          onPress={addRoutine}
-        >
-          Add Routine
-        </Button>
-        <Button
-          className="text-lg"
-          size="lg"
-          color="primary"
-          onPress={() => navigate(`/asd`)}
-        >
-          Test Not Found
-        </Button>
-        <Button
-          className="text-lg"
-          size="lg"
-          color="primary"
-          onPress={testFunction}
-        >
-          Update Settings
-        </Button>
-        <p>Settings Id: {userSettings?.id}</p>
-        <p>Active Routine Id: {userSettings?.active_routine_id}</p>
-      </div>
-    </div>
+    </>
   );
 }
