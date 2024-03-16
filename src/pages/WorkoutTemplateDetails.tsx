07c09d13c4a2384691560b8e22574e8d88e39dd0
Viktor <vikt.mag@gmail.com>
2024-03-16 13:05:53 +0100

    Update styling of newSetModal

--- src/pages/WorkoutTemplateDetails.tsx
+++ src/pages/WorkoutTemplateDetails.tsx
@@ -17,6 +17,7 @@ import {
   ModalFooter,
   Select,
   SelectItem,
+  ScrollShadow
 } from "@nextui-org/react";
 import Database from "tauri-plugin-sql-api";
 import LoadingSpinner from "../components/LoadingSpinner";
@@ -203,21 +204,22 @@ export default function WorkoutTemplateDetails() {
               </ModalHeader>
               <ModalBody>
                 {selectedExercise === undefined ? (
-                  <div className="h-[400px] flex flex-col gap-3">
+                  <div className="h-[400px] flex flex-col gap-2">
                     <Input
                       label="Search"
                       variant="faded"
                       placeholder="Type to search..."
+                      size="lg"
                       isClearable
                       value={filterQuery}
                       onValueChange={setFilterQuery}
                       startContent={<SearchIcon />}
                     />
-                    <div className="flex flex-col gap-1 px-1">
+                    <ScrollShadow className="flex flex-col gap-1">
                       {filteredExercises.map((exercise) => (
                         <button
                           key={exercise.id}
-                          className="flex flex-col justify-start items-start"
+                          className="flex flex-col justify-start items-start bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:bg-default-200 hover:border-default-400 focus:bg-default-200 focus:border-default-400"
                         >
                           <span className="text-md">{exercise.name}</span>
                           <span className="text-xs text-stone-500 text-left">
@@ -225,7 +227,7 @@ export default function WorkoutTemplateDetails() {
                           </span>
                         </button>
                       ))}
-                    </div>
+                    </ScrollShadow>
                   </div>
                 ) : (
                   <div className="h-[400px]">
