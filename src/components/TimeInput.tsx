52a4605ab0f8fc96c14602b73c6c4ccb03a6674e
Viktor <vikt.mag@gmail.com>
2024-03-19 05:08:18 +0100

    Add inputType Select element to TimeInput

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import { WorkoutSet } from "../typings";
+import { useState } from "react";
 
 type TimeInputProps = {
   value: WorkoutSet;
@@ -7,14 +8,39 @@ type TimeInputProps = {
 };
 
 export const TimeInput = ({ value, setValue }: TimeInputProps) => {
+  const [inputType, setInputType] = useState<string>("hhmmss");
   const handleChange = () => {
     setValue((prev) => ({ ...prev, time_in_seconds: 5 }));
   };
 
   return (
     <div className="flex flex-col justify-center items-center">
-      {value.time_in_seconds}
-      <Button onPress={handleChange}>Update</Button>
+      <div className="flex justify-between gap-2">
+        <div className="flex items-center">
+          {inputType === "hhmmss" && "HH:MM:SS"}
+          {inputType === "minutes" && "Minutes"}
+          {inputType === "seconds" && "Seconds"}
+        </div>
+        <Select
+          className="w-32"
+          label="Input Type"
+          size="sm"
+          variant="faded"
+          selectedKeys={[inputType]}
+          disallowEmptySelection={true}
+          onChange={(e) => setInputType(e.target.value)}
+        >
+          <SelectItem key="hhmmss" value={"hhmmss"}>
+            HH:MM:SS
+          </SelectItem>
+          <SelectItem key="minutes" value={"minutes"}>
+            Minutes
+          </SelectItem>
+          <SelectItem key="seconds" value={"seconds"}>
+            Seconds
+          </SelectItem>
+        </Select>
+      </div>
     </div>
   );
 };
