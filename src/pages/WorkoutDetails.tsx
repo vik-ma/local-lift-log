4524f6f77a343ea03f899f29215c0f5ee774e31a
Viktor <vikt.mag@gmail.com>
2024-03-24 02:28:03 +0100

    Add getWorkout function to WorkoutDetails

--- src/pages/WorkoutDetails.tsx
+++ src/pages/WorkoutDetails.tsx
@@ -1,13 +1,57 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Workout } from "../typings";
+import { LoadingSpinner } from "../components";
+import Database from "tauri-plugin-sql-api";
 
 export default function WorkoutDetails() {
+  const [workout, setWorkout] = useState<Workout>();
+  const [workoutDate, setWorkoutDate] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   const { id } = useParams();
+
+  useEffect(() => {
+    const getWorkout = async () => {
+      try {
+        const db = await Database.load(import.meta.env.VITE_DB);
+
+        const result = await db.select<Workout[]>(
+          "SELECT * FROM workouts WHERE id = $1",
+          [id]
+        );
+
+        if (result.length === 0) return;
+
+        const currentRoutine: Workout = result[0];
+
+        const formattedDate: string = new Date(
+          currentRoutine.date
+        ).toDateString();
+
+        setWorkout(currentRoutine);
+        setWorkoutDate(formattedDate);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getWorkout();
+  }, [id]);
+
   return (
     <>
-      <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
-        <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b">
-          {id}
-        </h1>
+      <div className="flex flex-col gap-4">
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <div className="flex justify-center">
+              <h1 className="text-2xl font-semibold">{workoutDate}</h1>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
