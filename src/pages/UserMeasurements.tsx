b7452cab3b3426d51447aaf8cd2cc374912edfb3
Viktor <vikt.mag@gmail.com>
2024-03-28 13:09:56 +0100

    Add layout to add UserWeight to UserMeasurements

--- src/pages/UserMeasurements.tsx
+++ src/pages/UserMeasurements.tsx
@@ -2,17 +2,24 @@ import { useState, useEffect } from "react";
 import { UserSettingsOptional, UserWeight } from "../typings";
 import { LoadingSpinner, WeightUnitDropdown } from "../components";
 import { GetDefaultUnitValues } from "../helpers";
+import { Button, Input } from "@nextui-org/react";
 
 export default function UserMeasurementsPage() {
   const [userSettings, setUserSettings] = useState<UserSettingsOptional>();
-  const [userWeights, setUserWeights] = useState<UserWeight>();
+  const [userWeights, setUserWeights] = useState<UserWeight[]>([]);
+  const [latestUserWeight, setLatestUserWeight] = useState<UserWeight>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [newWeightInput, setNewWeightInput] = useState<string>("");
+  const [newWeightUnit, setNewWeightUnit] = useState<string>("");
 
   useEffect(() => {
     const loadUserSettings = async () => {
       const settings: UserSettingsOptional | undefined =
         await GetDefaultUnitValues();
-      if (settings !== undefined) setUserSettings(settings);
+      if (settings !== undefined) {
+        setUserSettings(settings);
+        setNewWeightUnit(settings.default_unit_weight!);
+      }
       setIsLoading(false);
     };
 
@@ -30,8 +37,29 @@ export default function UserMeasurementsPage() {
         {isLoading ? (
           <LoadingSpinner />
         ) : (
-          <div>
-            <h2 className="text-2xl font-semibold">Body Weight</h2>
+          <div className="flex flex-col gap-4">
+            <h2 className="flex text-3xl font-semibold justify-center">
+              Body Weight
+            </h2>
+            <div className="flex justify-between gap-2 items-center">
+              <Input
+                value={newWeightInput}
+                label="Weight"
+                size="sm"
+                variant="faded"
+                onValueChange={(value) => setNewWeightInput(value)}
+                // isInvalid={}
+                isClearable
+              />
+              <WeightUnitDropdown
+                value={newWeightUnit}
+                actionMeasurements={setNewWeightUnit}
+                targetType="measurements"
+              />
+              <Button className="font-medium" color="success">
+                Add
+              </Button>
+            </div>
           </div>
         )}
       </div>
