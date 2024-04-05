f8481aec949307fdc48c49e1b68869c293e8adf1
Viktor <vikt.mag@gmail.com>
2024-04-05 16:59:37 +0200

    Fix CreateSetsFromWorkoutTemplate properly creating sets

--- src/helpers/Sets/CreateSetsFromWorkoutTemplate.tsx
+++ src/helpers/Sets/CreateSetsFromWorkoutTemplate.tsx
@@ -48,7 +48,7 @@ export const CreateSetsFromWorkoutTemplate = async (
               is_tracking_resistance_level, weight_unit, distance_unit, is_superset, is_dropset, multiset_values)
             VALUES
             ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, 
-              $21, $22, $23, $24, $25, $26, $27)`,
+              $21, $22, $23, $24, $25, $26)`,
         [
           set.workout_id,
           set.exercise_id,
