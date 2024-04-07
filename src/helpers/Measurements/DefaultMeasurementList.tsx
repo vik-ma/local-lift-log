a96ae1c7e0528e1352cd74b2311ad998d54e2e0a
Viktor <vikt.mag@gmail.com>
2024-04-07 10:52:17 +0200

    Add default_unit property to DefaultMeasurementList

--- src/helpers/Measurements/DefaultMeasurementList.tsx
+++ src/helpers/Measurements/DefaultMeasurementList.tsx
@@ -1,98 +1,123 @@
 type DefaultMeasurement = {
   name: string;
+  default_unit: string;
   measurement_type: string;
 };
 
-export const DefaultMeasurementList = (): DefaultMeasurement[] => {
+export const DefaultMeasurementList = (
+  isMetric: boolean
+): DefaultMeasurement[] => {
   const defaultMeasurementList: DefaultMeasurement[] = [];
 
   defaultMeasurementList.push({
     name: "Neck",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Chest",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Biceps (Right)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Biceps (Left)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Forearm (Right)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Forearm (Left)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Waist",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Hip",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Thigh (Right)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Thigh (Left)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Calf (Right)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
   defaultMeasurementList.push({
     name: "Calf (Left)",
+    default_unit: isMetric ? "cm" : "in",
     measurement_type: "Circumference",
   });
 
   defaultMeasurementList.push({
     name: "Chest",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Biceps",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Triceps",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Midaxillary",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Subscapular",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Suprailiac",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Abdominal",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Lower Back",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Thigh",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
   defaultMeasurementList.push({
     name: "Calf",
+    default_unit: "mm",
     measurement_type: "Caliper",
   });
 
