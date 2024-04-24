697d1ba3e21e2ba97e8d9af59d8e80eab4183d62
Viktor <vikt.mag@gmail.com>
2024-04-24 22:43:35 +0200

    Create CommentIcon SVG component asset

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -4,3 +4,4 @@ export { ChevronDownIcon } from "./ChevronDownIcon";
 export { SearchIcon } from "./SearchIcon";
 export { VerticalMenuIcon } from "./VerticalMenuIcon";
 export { CheckmarkIcon } from "./CheckmarkIcon";
+export { CommentIcon } from "./CommentIcon";
