240ab7d1f2405afbbe8066f1f6cbe50cbf9c92fc
Viktor <vikt.mag@gmail.com>
2024-05-02 15:38:46 +0200

    Add footer to TestPage

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,11 +1,76 @@
 export default function TestPage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
         <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b truncate">
           TEST
         </h1>
       </div>
+      <div className="flex flex-col bg-white border border-black overflow-auto mb-20">
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+        Test
+        <br />
+      </div>
+      <div className="fixed bottom-0 bg-red-500 h-20 w-[400px]"></div>
     </div>
   );
 }
