536f054703885bfce1ecf54e2a57a427a95c4099
Viktor <vikt.mag@gmail.com>
2024-05-02 23:55:01 +0200

    Modify ChevronDownIcon props slightly

--- src/assets/ChevronDownIcon.tsx
+++ src/assets/ChevronDownIcon.tsx
@@ -10,7 +10,6 @@ export const ChevronDownIcon = ({
   size,
   height,
   width,
-  ...props
 }: ChevronDownProps) => {
   return (
     <svg
@@ -19,11 +18,10 @@ export const ChevronDownIcon = ({
       viewBox="0 0 24 24"
       width={size || width || 24}
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
     >
       <path
         d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
-        stroke={fill}
+        stroke={fill || "#999999"}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeMiterlimit={10}
