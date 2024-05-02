a74585582968519d7f81c78c91333ac481856329
Viktor <vikt.mag@gmail.com>
2024-05-02 15:06:45 +0200

    Simplify definitions of defaultEquipmentWeightList in DefaultEquipmentWeightList

--- src/helpers/Presets/DefaultEquipmentWeightList.tsx
+++ src/helpers/Presets/DefaultEquipmentWeightList.tsx
@@ -7,33 +7,18 @@ type DefaultEquipmentWeight = {
 export const DefaultEquipmentWeightList = (
   isMetric: boolean
 ): DefaultEquipmentWeight[] => {
-  const defaultEquipmentWeightList: DefaultEquipmentWeight[] = [];
-
-  if (isMetric) {
-    // Metric Units
-    defaultEquipmentWeightList.push({
+  const defaultEquipmentWeightList: DefaultEquipmentWeight[] = [
+    {
       name: "Barbell",
-      weight: 20,
-      weight_unit: "kg",
-    });
-    defaultEquipmentWeightList.push({
+      weight: isMetric ? 20 : 45,
+      weight_unit: isMetric ? "kg" : "lbs",
+    },
+    {
       name: "Dumbbell",
-      weight: 2,
-      weight_unit: "kg",
-    });
-  } else {
-    // Imperial Units
-    defaultEquipmentWeightList.push({
-      name: "Barbell",
-      weight: 45,
-      weight_unit: "lbs",
-    });
-    defaultEquipmentWeightList.push({
-      name: "Dumbbell",
-      weight: 5,
-      weight_unit: "lbs",
-    });
-  }
+      weight: isMetric ? 2 : 5,
+      weight_unit: isMetric ? "kg" : "lbs",
+    },
+  ];
 
   return defaultEquipmentWeightList;
 };
