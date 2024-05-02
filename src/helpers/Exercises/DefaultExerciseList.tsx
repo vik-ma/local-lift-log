231dce325256282da23207cfafe6b80980d619f9
Viktor <vikt.mag@gmail.com>
2024-05-02 15:01:05 +0200

    Simplify definitions of variables in in DefaultExerciseList and DefaultMeasurementList

--- src/helpers/Exercises/DefaultExerciseList.tsx
+++ src/helpers/Exercises/DefaultExerciseList.tsx
@@ -4,48 +4,48 @@ type DefaultExercise = {
 };
 
 export const DefaultExerciseList = (): DefaultExercise[] => {
-  const defaultExerciseList: DefaultExercise[] = [];
-
-  defaultExerciseList.push({
-    name: "Bench Press",
-    exercise_group_set_string: "0,1",
-  });
-  defaultExerciseList.push({
-    name: "Hammer Curl",
-    exercise_group_set_string: "2,3",
-  });
-  defaultExerciseList.push({
-    name: "Lateral Raise",
-    exercise_group_set_string: "4",
-  });
-  defaultExerciseList.push({
-    name: "Deadlift",
-    exercise_group_set_string: "5,6,7,8,9,10,11",
-  });
-  defaultExerciseList.push({
-    name: "Calf Raise",
-    exercise_group_set_string: "12",
-  });
-  defaultExerciseList.push({
-    name: "Sit-Up",
-    exercise_group_set_string: "13",
-  });
-  defaultExerciseList.push({
-    name: "Crush Gripper",
-    exercise_group_set_string: "14",
-  });
-  defaultExerciseList.push({
-    name: "Neck Curl",
-    exercise_group_set_string: "15",
-  });
-  defaultExerciseList.push({
-    name: "Stationary Bike",
-    exercise_group_set_string: "16",
-  });
-  defaultExerciseList.push({
-    name: "Other",
-    exercise_group_set_string: "17",
-  });
+  const defaultExerciseList: DefaultExercise[] = [
+    {
+      name: "Bench Press",
+      exercise_group_set_string: "0,1",
+    },
+    {
+      name: "Hammer Curl",
+      exercise_group_set_string: "2,3",
+    },
+    {
+      name: "Lateral Raise",
+      exercise_group_set_string: "4",
+    },
+    {
+      name: "Deadlift",
+      exercise_group_set_string: "5,6,7,8,9,10,11",
+    },
+    {
+      name: "Calf Raise",
+      exercise_group_set_string: "12",
+    },
+    {
+      name: "Sit-Up",
+      exercise_group_set_string: "13",
+    },
+    {
+      name: "Crush Gripper",
+      exercise_group_set_string: "14",
+    },
+    {
+      name: "Neck Curl",
+      exercise_group_set_string: "15",
+    },
+    {
+      name: "Stationary Bike",
+      exercise_group_set_string: "16",
+    },
+    {
+      name: "Other",
+      exercise_group_set_string: "17",
+    },
+  ];
 
   return defaultExerciseList;
 };
--- src/helpers/Measurements/DefaultMeasurementList.tsx
+++ src/helpers/Measurements/DefaultMeasurementList.tsx
@@ -7,119 +7,119 @@ type DefaultMeasurement = {
 export const DefaultMeasurementList = (
   isMetric: boolean
 ): DefaultMeasurement[] => {
-  const defaultMeasurementList: DefaultMeasurement[] = [];
+  const defaultMeasurementList: DefaultMeasurement[] = [
+    {
+      name: "Neck",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Chest",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Biceps (Right)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Biceps (Left)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Forearm (Right)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Forearm (Left)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Waist",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Hip",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Thigh (Right)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Thigh (Left)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Calf (Right)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
+    {
+      name: "Calf (Left)",
+      default_unit: isMetric ? "cm" : "in",
+      measurement_type: "Circumference",
+    },
 
-  defaultMeasurementList.push({
-    name: "Neck",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Chest",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Biceps (Right)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Biceps (Left)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Forearm (Right)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Forearm (Left)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Waist",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Hip",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Thigh (Right)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Thigh (Left)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Calf (Right)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-  defaultMeasurementList.push({
-    name: "Calf (Left)",
-    default_unit: isMetric ? "cm" : "in",
-    measurement_type: "Circumference",
-  });
-
-  defaultMeasurementList.push({
-    name: "Chest",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Biceps",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Triceps",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Midaxillary",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Subscapular",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Suprailiac",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Abdominal",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Lower Back",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Thigh",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
-  defaultMeasurementList.push({
-    name: "Calf",
-    default_unit: "mm",
-    measurement_type: "Caliper",
-  });
+    {
+      name: "Chest",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Biceps",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Triceps",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Midaxillary",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Subscapular",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Suprailiac",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Abdominal",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Lower Back",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Thigh",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+    {
+      name: "Calf",
+      default_unit: "mm",
+      measurement_type: "Caliper",
+    },
+  ];
 
   return defaultMeasurementList;
 };
