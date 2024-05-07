5b39fd085dfcab9ba9d1b39e962e2488961b7b97
Viktor <vikt.mag@gmail.com>
2024-05-07 22:01:25 +0200

    Add color and size props to MinimizeIcon

--- src/assets/MinimizeIcon.tsx
+++ src/assets/MinimizeIcon.tsx
@@ -1,15 +1,19 @@
-export const MinimizeIcon = () => {
+type MinimizeIconProps = {
+  color?: string;
+  size?: number;
+};
+
+export const MinimizeIcon = ({ color, size }: MinimizeIconProps) => {
   return (
     <svg
-      fill="#000000"
-      width="64px"
-      height="64px"
+      width={size || 32}
+      height={size || 32}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M4,20l6-6M20,4l-6,6 M6 14 10 14 10 18 M18 10 14 10 14 6"
-        stroke="#000000"
+        stroke={color || "#555555"}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
