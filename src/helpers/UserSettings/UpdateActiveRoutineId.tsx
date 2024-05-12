768fe741ff4774f3d9282a6866a2a3c1ae9b9326
Viktor <vikt.mag@gmail.com>
2024-05-12 15:22:30 +0200

    Add validation to UpdateActiveRoutineId helper function

--- src/helpers/UserSettings/UpdateActiveRoutineId.tsx
+++ src/helpers/UserSettings/UpdateActiveRoutineId.tsx
@@ -1,20 +1,22 @@
 import Database from "tauri-plugin-sql-api";
 import { UserSettingsOptional } from "../../typings";
+import { IsNumberNegativeOrInfinity } from "..";
 
-export const UpdateActiveRoutineId = async (userSettings: UserSettingsOptional) => {
-    if (userSettings.active_routine_id === undefined) return;
+export const UpdateActiveRoutineId = async (
+  userSettings: UserSettingsOptional
+) => {
+  if (userSettings.active_routine_id === undefined) return;
 
-    try {
-      const db = await Database.load(import.meta.env.VITE_DB);
-  
-      db.execute(
-        "UPDATE user_settings SET active_routine_id = $1 WHERE id = $2",
-        [
-          userSettings.active_routine_id,
-          userSettings.id,
-        ]
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  if (IsNumberNegativeOrInfinity(userSettings.active_routine_id)) return;
+
+  try {
+    const db = await Database.load(import.meta.env.VITE_DB);
+
+    db.execute(
+      "UPDATE user_settings SET active_routine_id = $1 WHERE id = $2",
+      [userSettings.active_routine_id, userSettings.id]
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
