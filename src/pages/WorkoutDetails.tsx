068037a688efa66b3e325f9c558f63c364215a2c
Viktor <vikt.mag@gmail.com>
2024-05-13 17:49:55 +0200

    Add RPE, RIR and Resistance Level labels to Set List in WorkoutDetails

--- src/pages/WorkoutDetails.tsx
+++ src/pages/WorkoutDetails.tsx
@@ -1556,29 +1556,51 @@ export default function WorkoutDetails() {
                                       )}
                                     </div>
                                     {set.is_tracking_weight === 1 &&
-                                      set.weight > 0 && (
+                                      (set.weight > 0 ||
+                                        set.is_completed === 1) && (
                                         <span className="truncate max-w-16">
                                           {set.weight} {set.weight_unit}
                                         </span>
                                       )}
                                     {set.is_tracking_reps === 1 &&
-                                      set.reps > 0 && (
+                                      (set.reps > 0 ||
+                                        set.is_completed === 1) && (
                                         <span className="truncate max-w-16">
-                                          {set.reps} Rep{set.reps > 1 && "s"}
+                                          {set.reps} Rep{set.reps !== 1 && "s"}
                                         </span>
                                       )}
                                     {set.is_tracking_distance === 1 &&
-                                      set.distance > 0 && (
+                                      set.distance === 1 && (
                                         <span className="truncate max-w-16">
                                           {set.distance} {set.distance_unit}
                                         </span>
                                       )}
                                     {set.is_tracking_time === 1 &&
-                                      set.time_in_seconds > 0 && (
+                                      (set.time_in_seconds > 0 ||
+                                        set.is_completed === 1) && (
                                         <span className="truncate max-w-16">
                                           {FormatTimeInSecondsToHhmmssString(
                                             set.time_in_seconds
                                           )}
+                                        </span>
+                                      )}
+                                    {set.is_tracking_rpe === 1 &&
+                                      set.is_completed === 1 && (
+                                        <span className="truncate max-w-16">
+                                          RPE {set.rpe}
+                                        </span>
+                                      )}
+                                    {set.is_tracking_rir === 1 && (
+                                      <span className="truncate max-w-16">
+                                        {set.rir} RIR
+                                      </span>
+                                    )}
+                                    {set.is_tracking_resistance_level === 1 &&
+                                      (set.resistance_level > 0 ||
+                                        set.is_completed === 1) && (
+                                        <span className="truncate max-w-16">
+                                          Resistance Level{" "}
+                                          {set.resistance_level}
                                         </span>
                                       )}
                                     <div className="flex w-12 justify-end">
