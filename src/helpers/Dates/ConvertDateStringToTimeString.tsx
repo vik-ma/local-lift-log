f757b3bdf3060851eeb61c6f66fc2f631d5e167b
Viktor <vikt.mag@gmail.com>
2024-05-13 16:40:14 +0200

    Make ConvertDateStringToTimeString handle 12 hour format times

--- src/helpers/Dates/ConvertDateStringToTimeString.tsx
+++ src/helpers/Dates/ConvertDateStringToTimeString.tsx
@@ -1,5 +1,17 @@
-export const ConvertDateStringToTimeString = (dateString: string): string => {
+export const ConvertDateStringToTimeString = (
+  dateString: string,
+  is24hFormat: boolean
+): string => {
+  if (is24hFormat) {
+    const timeString: string = dateString.substring(16, 24);
+    return timeString;
+  }
+
   const date = new Date(dateString);
-  const timeString: string = date.toString().substring(16, 24);
+  const timeString = date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
   return timeString;
 };
