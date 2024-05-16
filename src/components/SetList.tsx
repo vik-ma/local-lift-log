b18e4476d21d7fc277a3e812463b5c8825a5049f
Viktor <vikt.mag@gmail.com>
2024-05-16 17:58:24 +0200

    Tweak styling of tracking values in SetList

--- src/components/SetList.tsx
+++ src/components/SetList.tsx
@@ -51,7 +51,7 @@ export const SetList = ({
             <div className="flex items-center w-[4.5rem]">
               <span>Set {index + 1}</span>
             </div>
-            <div className="flex flex-wrap justify-center w-full gap-x-5 px-1 gap-y-0.5 py-0.5">
+            <div className="flex flex-wrap justify-evenly w-full gap-x-5 px-1 gap-y-0.5 py-0.5">
               {set.is_tracking_weight === 1 &&
                 (set.weight > 0 || set.is_completed === 1) && (
                   <div className="flex gap-1">
