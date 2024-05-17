1724af3d6e4360c79c282c92d81677a87733837d
Viktor <vikt.mag@gmail.com>
2024-05-17 08:53:42 +0200

    Add 12h format to FormatDateTimeString helper function

--- src/helpers/Dates/FormatDateTimeString.tsx
+++ src/helpers/Dates/FormatDateTimeString.tsx
@@ -1,6 +1,25 @@
-export const FormatDateTimeString = (dateString: string): string => {
-  const formattedDate: string = new Date(dateString)
-    .toString()
-    .substring(0, 24);
+export const FormatDateTimeString = (
+  dateString: string,
+  is24hFormat: boolean
+): string => {
+  if (is24hFormat) {
+    const formattedDate: string = new Date(dateString)
+      .toString()
+      .substring(0, 24);
+    return formattedDate;
+  }
+
+  const formattedDate = new Date(dateString)
+    .toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    })
+    .replace(/,/g, "");
   return formattedDate;
 };
