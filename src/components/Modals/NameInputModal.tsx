f584a5e23f2c217fbafcc8285bb89c43cf949e8e
Viktor <vikt.mag@gmail.com>
2024-06-16 11:02:05 +0200

    Create NameInputModal component

---
+++ src/components/Modals/NameInputModal.tsx
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  useDisclosure,
+  Input,
+} from "@nextui-org/react";
+
+type NameInputModalProps = {
+  nameInputModal: ReturnType<typeof useDisclosure>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  header: string;
+  isNameValid: boolean;
+  buttonAction: () => void;
+};
+
+export const NameInputModal = ({
+  nameInputModal,
+  name,
+  setName,
+  header,
+  isNameValid,
+  buttonAction,
+}: NameInputModalProps) => {
+  return (
+    <Modal
+      isOpen={nameInputModal.isOpen}
+      onOpenChange={nameInputModal.onOpenChange}
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader>{header}</ModalHeader>
+            <ModalBody>
+              <div className="h-16">
+                <Input
+                  value={name}
+                  isInvalid={!isNameValid}
+                  label="Name"
+                  errorMessage={!isNameValid && "Name can't be empty"}
+                  variant="faded"
+                  onValueChange={(value) => setName(value)}
+                  isRequired
+                  isClearable
+                />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="success" variant="light" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="success" onPress={() => buttonAction()}>
+                Add
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -27,3 +27,4 @@ export { WorkoutTemplateListModal } from "./Modals/WorkoutTemplateListModal";
 export { UserWeightModal } from "./Modals/UserWeightModal";
 export { UserMeasurementModal } from "./Modals/UserMeasurementModal";
 export { MeasurementModal } from "./Modals/MeasurementModal";
+export { NameInputModal } from "./Modals/NameInputModal";
