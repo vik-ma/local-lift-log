4e40b6fa2ae9bb1e2a68a5af7008054d603ac84e
Viktor <vikt.mag@gmail.com>
2024-06-20 12:05:10 +0200

    Modify values of DefaultDistances

--- src/helpers/Constants/DefaultDistances.tsx
+++ src/helpers/Constants/DefaultDistances.tsx
@@ -5,33 +5,23 @@ type DefaultDistance = {
 };
 
 export const DefaultDistances = (isMetric: boolean): DefaultDistance[] => {
-  const DEFAULT_DISTANCE_LIST: DefaultDistance[] = [];
-
-  if (isMetric) {
-    // Metric Units
-    DEFAULT_DISTANCE_LIST.push({
-      name: "5 Kilometers",
-      distance: 5,
-      distance_unit: "km",
-    });
-    DEFAULT_DISTANCE_LIST.push({
-      name: "10 Kilometers",
-      distance: 10,
-      distance_unit: "km",
-    });
-  } else {
-    // Imperial Units
-    DEFAULT_DISTANCE_LIST.push({
-      name: "3 Miles",
-      distance: 3,
-      distance_unit: "mi",
-    });
-    DEFAULT_DISTANCE_LIST.push({
-      name: "6 Miles",
-      distance: 6,
-      distance_unit: "mi",
-    });
-  }
+  const DEFAULT_DISTANCE_LIST: DefaultDistance[] = [
+    {
+      name: "5K",
+      distance: isMetric ? 5 : 3.1,
+      distance_unit: isMetric ? "km" : "mi",
+    },
+    {
+      name: "10K",
+      distance: isMetric ? 10 : 6.2,
+      distance_unit: isMetric ? "km" : "mi",
+    },
+    {
+      name: "1 Mile",
+      distance: isMetric ? 1.6 : 1,
+      distance_unit: isMetric ? "km" : "mi",
+    },
+  ];
 
   Object.freeze(DEFAULT_DISTANCE_LIST);
 
