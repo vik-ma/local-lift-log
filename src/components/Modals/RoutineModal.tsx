ca7ceb921e0c56c88402a45a6122422c6f9b1472
Viktor <vikt.mag@gmail.com>
2024-06-24 19:13:58 +0200

    Tweak layout of RoutineModal

--- src/components/Modals/RoutineModal.tsx
+++ src/components/Modals/RoutineModal.tsx
@@ -71,62 +71,64 @@ export const RoutineModal = ({
               {routine.id === 0 ? "New" : "Edit"} Routine
             </ModalHeader>
             <ModalBody>
-              <div className="flex flex-col gap-0.5">
-                <Input
-                  className="h-[5rem]"
-                  value={routine.name}
-                  isInvalid={!isRoutineNameValid}
-                  label="Name"
-                  errorMessage={!isRoutineNameValid && "Name can't be empty"}
-                  variant="faded"
-                  onValueChange={(value) =>
-                    setRoutine((prev) => ({ ...prev, name: value }))
-                  }
-                  isRequired
-                  isClearable
-                />
-                <Input
-                  value={routine.note ?? ""}
-                  label="Note"
-                  variant="faded"
-                  onValueChange={(value) =>
-                    setRoutine((prev) => ({ ...prev, note: value }))
-                  }
-                  isClearable
-                />
-              </div>
-              <div className="flex justify-between items-center px-1 gap-4">
-                <RadioGroup
-                  value={routine.is_schedule_weekly ? "weekly" : "custom"}
-                  onValueChange={(value) => handleScheduleTypeChange(value)}
-                  defaultValue="weekly"
-                  label="Schedule Type"
-                >
-                  <Radio value="weekly">Weekly</Radio>
-                  <Radio value="custom">Custom</Radio>
-                </RadioGroup>
-                <Select
-                  isRequired
-                  size="lg"
-                  variant="faded"
-                  label="Number of days in schedule"
-                  labelPlacement="outside"
-                  placeholder="Select number of days"
-                  selectedKeys={[routine.num_days_in_schedule.toString()]}
-                  onChange={handleNumDaysInScheduleChange}
-                  className={
-                    routine.is_schedule_weekly
-                      ? "hidden max-w-[240px]"
-                      : " max-w-[240px]"
-                  }
-                  disallowEmptySelection
-                >
-                  {numDaysInScheduleOptions.map((number) => (
-                    <SelectItem key={number} value={number}>
-                      {number.toString()}
-                    </SelectItem>
-                  ))}
-                </Select>
+              <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-0.5">
+                  <Input
+                    className="h-[5rem]"
+                    value={routine.name}
+                    isInvalid={!isRoutineNameValid}
+                    label="Name"
+                    errorMessage={!isRoutineNameValid && "Name can't be empty"}
+                    variant="faded"
+                    onValueChange={(value) =>
+                      setRoutine((prev) => ({ ...prev, name: value }))
+                    }
+                    isRequired
+                    isClearable
+                  />
+                  <Input
+                    value={routine.note ?? ""}
+                    label="Note"
+                    variant="faded"
+                    onValueChange={(value) =>
+                      setRoutine((prev) => ({ ...prev, note: value }))
+                    }
+                    isClearable
+                  />
+                </div>
+                <div className="flex justify-between items-center px-1 py-0.5 gap-4">
+                  <RadioGroup
+                    value={routine.is_schedule_weekly ? "weekly" : "custom"}
+                    onValueChange={(value) => handleScheduleTypeChange(value)}
+                    defaultValue="weekly"
+                    label="Schedule Type"
+                  >
+                    <Radio value="weekly">Weekly</Radio>
+                    <Radio value="custom">Custom</Radio>
+                  </RadioGroup>
+                  <Select
+                    isRequired
+                    size="lg"
+                    variant="faded"
+                    label="Number of days in schedule"
+                    labelPlacement="outside"
+                    placeholder="Select number of days"
+                    selectedKeys={[routine.num_days_in_schedule.toString()]}
+                    onChange={handleNumDaysInScheduleChange}
+                    className={
+                      routine.is_schedule_weekly
+                        ? "hidden max-w-[240px]"
+                        : " max-w-[240px]"
+                    }
+                    disallowEmptySelection
+                  >
+                    {numDaysInScheduleOptions.map((number) => (
+                      <SelectItem key={number} value={number}>
+                        {number.toString()}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                </div>
               </div>
             </ModalBody>
             <ModalFooter>
