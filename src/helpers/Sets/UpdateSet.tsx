e03db86179d417b36a6df93eed2943fa0660de73
Viktor <vikt.mag@gmail.com>
2024-06-27 16:56:01 +0200

    Fix UpdateSet function for new sets columns

--- src/helpers/Sets/UpdateSet.tsx
+++ src/helpers/Sets/UpdateSet.tsx
@@ -10,11 +10,11 @@ export const UpdateSet = async (set: WorkoutSet): Promise<boolean> => {
         workout_id = $1, exercise_id = $2, is_template = $3, workout_template_id = $4,
         note = $5, comment = $6, is_completed = $7, time_completed = $8, is_warmup = $9, 
         weight = $10, reps = $11, distance = $12, time_in_seconds = $13, rir = $14, rpe = $15, 
-        resistance_level = $16, is_tracking_weight = $17, is_tracking_reps = $18, 
-        is_tracking_rir = $19, is_tracking_rpe = $20, is_tracking_time = $21, 
-        is_tracking_distance = $22, is_tracking_resistance_level = $23, weight_unit = $24, 
-        distance_unit = $25, multiset_id = $26 
-        WHERE id = $27`,
+        resistance_level = $16, partial_reps = $17, is_tracking_weight = $18, is_tracking_reps = $19, 
+        is_tracking_rir = $20, is_tracking_rpe = $21, is_tracking_time = $22, 
+        is_tracking_distance = $23, is_tracking_resistance_level = $24, is_tracking_partial_reps = $25, 
+        weight_unit = $26, distance_unit = $27, multiset_id = $28 
+        WHERE id = $29`,
       [
         set.workout_id,
         set.exercise_id,
@@ -32,6 +32,7 @@ export const UpdateSet = async (set: WorkoutSet): Promise<boolean> => {
         set.rir,
         set.rpe,
         set.resistance_level,
+        set.partial_reps,
         set.is_tracking_weight,
         set.is_tracking_reps,
         set.is_tracking_rir,
@@ -39,6 +40,7 @@ export const UpdateSet = async (set: WorkoutSet): Promise<boolean> => {
         set.is_tracking_time,
         set.is_tracking_distance,
         set.is_tracking_resistance_level,
+        set.is_tracking_partial_reps,
         set.weight_unit,
         set.distance_unit,
         set.multiset_id,
