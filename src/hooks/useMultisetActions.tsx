6914816ce590b1fd00608c327992ceaae96cc410
Viktor <vikt.mag@gmail.com>
2024-07-07 02:18:37 +0200

    Remove setOperationType property from useMultisetActions

--- src/hooks/useMultisetActions.tsx
+++ src/hooks/useMultisetActions.tsx
@@ -1,17 +1,15 @@
 import { useDisclosure } from "@nextui-org/react";
-import { Multiset, WorkoutSet, MultisetOperationType } from "../typings";
+import { Multiset, WorkoutSet } from "../typings";
 
 type UseMultisetActionsProps = {
   setOperatingMultiset: React.Dispatch<React.SetStateAction<Multiset>>;
   setOperatingSet: React.Dispatch<React.SetStateAction<WorkoutSet>>;
-  setOperationType: React.Dispatch<React.SetStateAction<MultisetOperationType>>;
   deleteModal: ReturnType<typeof useDisclosure>;
 };
 
 export const useMultisetActions = ({
   setOperatingMultiset,
   setOperatingSet,
-  setOperationType,
   deleteModal,
 }: UseMultisetActionsProps) => {
   const handleMultisetSetOptionSelection = (
@@ -22,18 +20,14 @@ export const useMultisetActions = ({
     if (key === "edit-set") {
       setOperatingSet(set);
       setOperatingMultiset(multiset);
-      setOperationType("edit-set");
     } else if (key === "delete-set") {
       setOperatingSet(set);
       setOperatingMultiset(multiset);
-      setOperationType("delete-set");
       deleteModal.onOpen();
     } else if (key === "change-exercise") {
       setOperatingSet(set);
-      setOperationType("change-exercise");
     } else if (key === "reassign-exercise") {
       setOperatingSet(set);
-      setOperationType("reassign-exercise");
     }
   };
 
