24ef9df623530c21adf6647c1ac1f291457c7146
Viktor <vikt.mag@gmail.com>
2024-07-11 03:09:06 +0200

    Make SetList component work with Multisets

--- src/components/SetList.tsx
+++ src/components/SetList.tsx
@@ -40,164 +40,164 @@ export const SetList = ({
 }: SetListProps) => {
   return (
     <>
-      {groupedSet.setList.map((set, index) => (
-        <div
-          className={
-            set.id === activeSetId
-              ? "flex flex-col pl-2 bg-yellow-100 text-yellow-600 text-sm font-medium break-words cursor-pointer"
-              : "flex flex-col pl-2 text-sm font-medium break-words cursor-pointer hover:bg-stone-100"
-          }
-          key={set.id}
-          onClick={() =>
-            clickSetAction(set, index, groupedSet.exerciseList[0])
-          }
-        >
-          <div className="flex justify-between items-center">
-            <div className="flex items-center w-[4.5rem]">
-              <span className="text-stone-500">Set {index + 1}</span>
-            </div>
-            <div className="flex flex-wrap justify-evenly w-full gap-x-5 px-1 gap-y-0.5 py-0.5">
-              {set.is_tracking_weight === 1 &&
-                (set.weight > 0 || set.is_completed === 1) && (
-                  <div className="flex gap-1">
-                    <span className="truncate max-w-12">{set.weight}</span>
-                    <span>{set.weight_unit}</span>
-                  </div>
-                )}
-              {set.is_tracking_reps === 1 &&
-                (set.reps > 0 || set.is_completed === 1) && (
-                  <div className="flex gap-1">
-                    <span className="truncate max-w-12">{set.reps}</span>
-                    <span>
-                      Rep
-                      {set.reps !== 1 && "s"}
+      {groupedSet.setList.map((set, index) => {
+        const exercise = groupedSet.isMultiset
+          ? groupedSet.exerciseList[index]
+          : groupedSet.exerciseList[0];
+        return (
+          <div
+            className={
+              set.id === activeSetId
+                ? "flex flex-col pl-2 bg-yellow-100 text-yellow-600 text-sm font-medium break-words cursor-pointer"
+                : "flex flex-col pl-2 text-sm font-medium break-words cursor-pointer hover:bg-stone-100"
+            }
+            key={set.id}
+            onClick={() => clickSetAction(set, index, exercise)}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex items-center w-[4.5rem]">
+                <span className="text-stone-500">Set {index + 1}</span>
+              </div>
+              <div className="flex flex-wrap justify-evenly w-full gap-x-5 px-1 gap-y-0.5 py-0.5">
+                {set.is_tracking_weight === 1 &&
+                  (set.weight > 0 || set.is_completed === 1) && (
+                    <div className="flex gap-1">
+                      <span className="truncate max-w-12">{set.weight}</span>
+                      <span>{set.weight_unit}</span>
+                    </div>
+                  )}
+                {set.is_tracking_reps === 1 &&
+                  (set.reps > 0 || set.is_completed === 1) && (
+                    <div className="flex gap-1">
+                      <span className="truncate max-w-12">{set.reps}</span>
+                      <span>
+                        Rep
+                        {set.reps !== 1 && "s"}
+                      </span>
+                    </div>
+                  )}
+                {set.is_tracking_distance === 1 &&
+                  (set.distance > 0 || set.is_completed === 1) && (
+                    <div className="flex gap-1">
+                      <span className="truncate max-w-12">{set.distance}</span>
+                      <span>{set.distance_unit}</span>
+                    </div>
+                  )}
+                {set.is_tracking_time === 1 &&
+                  (set.time_in_seconds > 0 || set.is_completed === 1) && (
+                    <span className="truncate max-w-16">
+                      {FormatTimeInSecondsToHhmmssString(set.time_in_seconds)}
                     </span>
-                  </div>
-                )}
-              {set.is_tracking_distance === 1 &&
-                (set.distance > 0 || set.is_completed === 1) && (
+                  )}
+                {set.is_tracking_rpe === 1 && set.is_completed === 1 && (
                   <div className="flex gap-1">
-                    <span className="truncate max-w-12">{set.distance}</span>
-                    <span>{set.distance_unit}</span>
+                    <span>RPE</span>
+                    <span className="truncate max-w-4">{set.rpe}</span>
                   </div>
                 )}
-              {set.is_tracking_time === 1 &&
-                (set.time_in_seconds > 0 || set.is_completed === 1) && (
-                  <span className="truncate max-w-16">
-                    {FormatTimeInSecondsToHhmmssString(set.time_in_seconds)}
-                  </span>
-                )}
-              {set.is_tracking_rpe === 1 && set.is_completed === 1 && (
-                <div className="flex gap-1">
-                  <span>RPE</span>
-                  <span className="truncate max-w-4">{set.rpe}</span>
-                </div>
-              )}
-              {set.is_tracking_rir === 1 && set.rir > -1 && (
-                <div className="flex gap-1">
-                  <span className="truncate max-w-10">{set.rir}</span>
-                  <span>RIR</span>
-                </div>
-              )}
-              {set.is_tracking_resistance_level === 1 &&
-                (set.resistance_level > 0 || set.is_completed === 1) && (
+                {set.is_tracking_rir === 1 && set.rir > -1 && (
                   <div className="flex gap-1">
-                    <span>Resistance Level</span>
-                    <span className="truncate max-w-12">
-                      {set.resistance_level}
-                    </span>
+                    <span className="truncate max-w-10">{set.rir}</span>
+                    <span>RIR</span>
                   </div>
                 )}
-              {set.is_tracking_partial_reps === 1 &&
-                (set.partial_reps > 0 || set.is_completed === 1) && (
-                  <div className="flex gap-1">
-                    <span className="truncate max-w-10">
-                      {set.partial_reps}
-                    </span>
-                    <span>
-                      Partial Rep
-                      {set.partial_reps !== 1 && "s"}
-                    </span>
+                {set.is_tracking_resistance_level === 1 &&
+                  (set.resistance_level > 0 || set.is_completed === 1) && (
+                    <div className="flex gap-1">
+                      <span>Resistance Level</span>
+                      <span className="truncate max-w-12">
+                        {set.resistance_level}
+                      </span>
+                    </div>
+                  )}
+                {set.is_tracking_partial_reps === 1 &&
+                  (set.partial_reps > 0 || set.is_completed === 1) && (
+                    <div className="flex gap-1">
+                      <span className="truncate max-w-10">
+                        {set.partial_reps}
+                      </span>
+                      <span>
+                        Partial Rep
+                        {set.partial_reps !== 1 && "s"}
+                      </span>
+                    </div>
+                  )}
+              </div>
+              <div
+                className={
+                  isTemplate
+                    ? "flex w-[6rem] items-center justify-end"
+                    : "flex w-[8.5rem] items-center justify-end"
+                }
+              >
+                {((!isTemplate && set.comment !== null) ||
+                  (isTemplate && set.note !== null)) && (
+                  <div className={isTemplate ? "" : "pr-1"}>
+                    <Button
+                      aria-label={`Toggle ${exercise.name} Set ${index} Comment`}
+                      isIconOnly
+                      size="sm"
+                      radius="lg"
+                      variant="light"
+                      onPress={() =>
+                        clickCommentButtonAction(exercise.id, index)
+                      }
+                    >
+                      <CommentIcon size={21} />
+                    </Button>
                   </div>
                 )}
-            </div>
-            <div
-              className={
-                isTemplate
-                  ? "flex w-[6rem] items-center justify-end"
-                  : "flex w-[8.5rem] items-center justify-end"
-              }
-            >
-              {((!isTemplate && set.comment !== null) ||
-                (isTemplate && set.note !== null)) && (
-                <div className={isTemplate ? "" : "pr-1"}>
-                  <Button
-                    aria-label={`Toggle ${groupedSet.exerciseList[0].name} Set ${index} Comment`}
-                    isIconOnly
-                    size="sm"
-                    radius="lg"
-                    variant="light"
-                    onPress={() =>
-                      clickCommentButtonAction(
-                        groupedSet.exerciseList[0].id,
-                        index
+                {!isTemplate && (
+                  <CheckmarkIcon isChecked={set.is_completed === 1} size={18} />
+                )}
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button
+                      aria-label={`Toggle ${exercise.name} Set ${index} Options Menu`}
+                      isIconOnly
+                      className="z-1"
+                      size="sm"
+                      radius="lg"
+                      variant="light"
+                    >
+                      <VerticalMenuIcon size={14} />
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    aria-label={`Option Menu For ${exercise.name} Set ${index}`}
+                    onAction={(key) =>
+                      optionsSelectionAction(
+                        key as string,
+                        set,
+                        index,
+                        exercise
                       )
                     }
                   >
-                    <CommentIcon size={21} />
-                  </Button>
-                </div>
-              )}
-              {!isTemplate && (
-                <CheckmarkIcon isChecked={set.is_completed === 1} size={18} />
-              )}
-              <Dropdown>
-                <DropdownTrigger>
-                  <Button
-                    aria-label={`Toggle ${groupedSet.exerciseList[0].name} Set ${index} Options Menu`}
-                    isIconOnly
-                    className="z-1"
-                    size="sm"
-                    radius="lg"
-                    variant="light"
-                  >
-                    <VerticalMenuIcon size={14} />
-                  </Button>
-                </DropdownTrigger>
-                <DropdownMenu
-                  aria-label={`Option Menu For ${groupedSet.exerciseList[0].name} Set ${index}`}
-                  onAction={(key) =>
-                    optionsSelectionAction(
-                      key as string,
-                      set,
-                      index,
-                      groupedSet.exerciseList[0]
-                    )
-                  }
-                >
-                  <DropdownItem key="edit">Edit</DropdownItem>
-                  <DropdownItem
-                    className={set.is_completed === 0 ? "hidden" : ""}
-                    key="update-completed-set-time"
-                  >
-                    Change Time Completed
-                  </DropdownItem>
-                  <DropdownItem key="delete-set" className="text-danger">
-                    {isTemplate ? "Remove" : "Delete"}
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
+                    <DropdownItem key="edit">Edit</DropdownItem>
+                    <DropdownItem
+                      className={set.is_completed === 0 ? "hidden" : ""}
+                      key="update-completed-set-time"
+                    >
+                      Change Time Completed
+                    </DropdownItem>
+                    <DropdownItem key="delete-set" className="text-danger">
+                      {isTemplate ? "Remove" : "Delete"}
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
             </div>
+            {shownSetListComments[groupedSet.exerciseList[0].id]?.has(
+              index
+            ) && (
+              <span className="text-stone-400 pb-1 pr-2">
+                {isTemplate ? `${set.note}` : `${set.comment}`}
+              </span>
+            )}
           </div>
-          {shownSetListComments[groupedSet.exerciseList[0].id]?.has(
-            index
-          ) && (
-            <span className="text-stone-400 pb-1 pr-2">
-              {isTemplate ? `${set.note}` : `${set.comment}`}
-            </span>
-          )}
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
