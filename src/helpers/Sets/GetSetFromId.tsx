b3c9a0e9e39073ae0de65cfaa59aa3cd872b87a5
Viktor <vikt.mag@gmail.com>
2024-08-01 19:10:30 +0200

    Change invalid exercise_name in GetSetFromId helper function

--- src/helpers/Sets/GetSetFromId.tsx
+++ src/helpers/Sets/GetSetFromId.tsx
@@ -27,7 +27,7 @@ export const GetSetFromId = async (
 
     if (exerciseName.length === 0) {
       set.hasInvalidExerciseId = true;
-      set.exercise_name = "Unknown";
+      set.exercise_name = "Unknown Exercise";
     } else {
       set.exercise_name = exerciseName[0].name;
     }
