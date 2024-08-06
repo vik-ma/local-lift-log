93d21a6e630f1695810640968a73ee58cf0942cd
Viktor <vikt.mag@gmail.com>
2024-08-06 16:29:30 +0200

    Add toggleFavorite button to ExerciseModalList

--- src/components/ExerciseModalList.tsx
+++ src/components/ExerciseModalList.tsx
@@ -1,5 +1,5 @@
-import { Input, ScrollShadow } from "@nextui-org/react";
-import { SearchIcon } from "../assets";
+import { Input, ScrollShadow, Button } from "@nextui-org/react";
+import { SearchIcon, FavoriteIcon } from "../assets";
 import { Exercise, UseExerciseListReturnType } from "../typings";
 import { Link } from "react-router-dom";
 
@@ -12,7 +12,8 @@ export const ExerciseModalList = ({
   handleClickExercise,
   exerciseList,
 }: ExerciseModalListProps) => {
-  const { filterQuery, setFilterQuery, filteredExercises } = exerciseList;
+  const { filterQuery, setFilterQuery, filteredExercises, toggleFavorite } =
+    exerciseList;
 
   return (
     <div className="h-[400px] flex flex-col gap-2">
@@ -27,18 +28,40 @@ export const ExerciseModalList = ({
       />
       <ScrollShadow className="flex flex-col gap-1">
         {filteredExercises.map((exercise) => (
-          <button
+          <div
             key={exercise.id}
-            className="flex flex-col justify-start items-start bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:bg-default-200 hover:border-default-400 focus:bg-default-200 focus:border-default-400"
-            onClick={() => handleClickExercise(exercise)}
+            className="flex flex-row justify-between items-center gap-1 bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:border-default-400 focus:bg-default-200 focus:border-default-400"
           >
-            <span className="text-base max-w-full truncate">
-              {exercise.name}
-            </span>
-            <span className="text-xs text-stone-500 text-left">
-              {exercise.formattedGroupString}
-            </span>
-          </button>
+            <button
+              className="flex flex-col justify-start items-start"
+              onClick={() => handleClickExercise(exercise)}
+            >
+              <span className="w-[20.5rem] truncate text-left">
+                {exercise.name}
+              </span>
+              <span className="text-xs text-stone-500 text-left">
+                {exercise.formattedGroupString}
+              </span>
+            </button>
+            <div className="flex items-center">
+              <Button
+                aria-label={
+                  exercise.is_favorite
+                    ? `Unset Favorite For ${exercise.name}`
+                    : `Set Favorite For ${exercise.name}`
+                }
+                isIconOnly
+                className="z-1"
+                size="sm"
+                color={exercise.is_favorite ? "primary" : "default"}
+                radius="lg"
+                variant="light"
+                onPress={() => toggleFavorite(exercise)}
+              >
+                <FavoriteIcon isChecked={!!exercise.is_favorite} size={28} />
+              </Button>
+            </div>
+          </div>
         ))}
         {filteredExercises.length === 0 && (
           <div className="flex flex-col items-center justify-center text-stone-500 py-2">
