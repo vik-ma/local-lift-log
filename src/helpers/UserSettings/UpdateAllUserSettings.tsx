b79ca01bc49d47c7f047c7fa0cf5e652023837ac
Viktor <vikt.mag@gmail.com>
2024-08-15 12:53:37 +0200

    Update UpdateAllUserSettings helper function with new columns in user_settings table

--- src/helpers/UserSettings/UpdateAllUserSettings.tsx
+++ src/helpers/UserSettings/UpdateAllUserSettings.tsx
@@ -16,8 +16,9 @@ export const UpdateAllUserSettings = async (
       default_unit_weight = $3, default_unit_distance = $4, default_time_input = $5,
       default_unit_measurement = $6, active_tracking_measurements = $7, locale = $8,
       clock_style = $9, time_input_behavior_hhmmss = $10, time_input_behavior_mmss = $11,
-      show_workout_rating = $12
-      WHERE id = $13`,
+      show_workout_rating = $12, default_increment_weight = $13, default_increment_weight = $14, 
+      default_increment_weight = $15, default_increment_weight = $16 
+      WHERE id = $17`,
       [
         userSettings.show_timestamp_on_completed_set,
         userSettings.active_routine_id,
@@ -31,6 +32,10 @@ export const UpdateAllUserSettings = async (
         userSettings.time_input_behavior_hhmmss,
         userSettings.time_input_behavior_mmss,
         userSettings.show_workout_rating,
+        userSettings.default_increment_weight,
+        userSettings.default_increment_distance,
+        userSettings.default_increment_time,
+        userSettings.default_increment_resistance_level,
         userSettings.id,
       ]
     );
