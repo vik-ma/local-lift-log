96d645a7580c4f66ef4be9057aa9c5802141aee6
Viktor <vikt.mag@gmail.com>
2024-08-18 14:49:24 +0200

    Tweak default size and color of PlusIcon and MinusIcon

--- src/assets/MinusIcon.tsx
+++ src/assets/MinusIcon.tsx
@@ -6,13 +6,13 @@ type MinusIconProps = {
 export const MinusIcon = ({ color, size }: MinusIconProps) => {
   return (
     <svg
-      width={size || 24}
-      height={size || 24}
+      width={size || 32}
+      height={size || 32}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill={color || "#808080"}
+        fill={color || "#999"}
         d="M6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12Z"
       ></path>
     </svg>
