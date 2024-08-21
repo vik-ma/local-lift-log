91bfd0b88ee3f77f47bee1f8cd6ff063a7555fa4
Viktor <vikt.mag@gmail.com>
2024-08-21 16:48:18 +0200

    Add currentWorkoutId parameter to GetSetsOfLastCompletedExercise helper function

--- src/helpers/Sets/GetSetsOfLastCompletedExercise.tsx
+++ src/helpers/Sets/GetSetsOfLastCompletedExercise.tsx
@@ -2,7 +2,8 @@ import { WorkoutSet } from "../../typings";
 import Database from "tauri-plugin-sql-api";
 
 export const GetSetsOfLastCompletedExercise = async (
-  exerciseId: number
+  exerciseId: number,
+  currentWorkoutId: number
 ): Promise<WorkoutSet[]> => {
   try {
     const db = await Database.load(import.meta.env.VITE_DB);
@@ -12,11 +13,13 @@ export const GetSetsOfLastCompletedExercise = async (
       FROM sets
       WHERE exercise_id = $1
       AND workout_id = (
-      SELECT MAX(workout_id) FROM sets 
+      SELECT MAX(workout_id) 
+      FROM sets 
       WHERE exercise_id = $1
+      AND workout_id != $2
       )
       AND is_completed = 1;`,
-      [exerciseId]
+      [exerciseId, currentWorkoutId]
     );
 
     return result;
