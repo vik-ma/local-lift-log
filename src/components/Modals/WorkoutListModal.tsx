5ac91331c9da3d397013276d7609085e9291608a
Viktor <vikt.mag@gmail.com>
2024-08-23 19:31:02 +0200

    Add label for empty workout list in WorkoutListModal

--- src/components/Modals/WorkoutListModal.tsx
+++ src/components/Modals/WorkoutListModal.tsx
@@ -51,55 +51,62 @@ export const WorkoutListModal = ({
                     </Button>
                   </div>
                 )}
-                <ScrollShadow className="flex flex-col gap-1">
-                  <div className="flex flex-col gap-1 w-full">
-                    {workouts.map((workout) => (
-                      <div
-                        key={workout.id}
-                        className="flex cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:border-default-400 focus:bg-default-200 focus:border-default-400"
-                        onClick={() => onClickAction(workout)}
-                      >
-                        <div className="flex gap-1 justify-between items-center w-full">
-                          <div className="flex flex-col justify-start items-start">
-                            <span className="w-[10.5rem] truncate text-left">
-                              {workout.date}
-                            </span>
-                            {workout.numSets! > 0 ? (
-                              <span className="text-xs text-secondary text-left">
-                                {FormatNumItemsString(
-                                  workout.numExercises,
-                                  "Exercise"
-                                )}
-                                , {FormatNumItemsString(workout.numSets, "Set")}
+                {workouts.length > 0 ? (
+                  <ScrollShadow className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 w-full">
+                      {workouts.map((workout) => (
+                        <div
+                          key={workout.id}
+                          className="flex cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:border-default-400 focus:bg-default-200 focus:border-default-400"
+                          onClick={() => onClickAction(workout)}
+                        >
+                          <div className="flex gap-1 justify-between items-center w-full">
+                            <div className="flex flex-col justify-start items-start">
+                              <span className="w-[10.5rem] truncate text-left">
+                                {workout.date}
                               </span>
-                            ) : (
-                              <span className="text-xs text-stone-400 text-left">
-                                Empty
-                              </span>
-                            )}
-                            <span
-                              className={
-                                userSettings.show_workout_rating === 1
-                                  ? "w-[16.5rem] break-all text-xs text-stone-500 text-left"
-                                  : "w-[21.5rem] break-all text-xs text-stone-500 text-left"
-                              }
-                            >
-                              {workout.note}
-                            </span>
-                          </div>
-                          {userSettings.show_workout_rating === 1 && (
-                            <div className="flex flex-col w-[4.5rem] text-center text-sm text-stone-500">
-                              <span>Rating</span>
-                              <span className="font-semibold">
-                                {workoutRatingMap[workout.rating].span}
+                              {workout.numSets! > 0 ? (
+                                <span className="text-xs text-secondary text-left">
+                                  {FormatNumItemsString(
+                                    workout.numExercises,
+                                    "Exercise"
+                                  )}
+                                  ,{" "}
+                                  {FormatNumItemsString(workout.numSets, "Set")}
+                                </span>
+                              ) : (
+                                <span className="text-xs text-stone-400 text-left">
+                                  Empty
+                                </span>
+                              )}
+                              <span
+                                className={
+                                  userSettings.show_workout_rating === 1
+                                    ? "w-[16.5rem] break-all text-xs text-stone-500 text-left"
+                                    : "w-[21.5rem] break-all text-xs text-stone-500 text-left"
+                                }
+                              >
+                                {workout.note}
                               </span>
                             </div>
-                          )}
+                            {userSettings.show_workout_rating === 1 && (
+                              <div className="flex flex-col w-[4.5rem] text-center text-sm text-stone-500">
+                                <span>Rating</span>
+                                <span className="font-semibold">
+                                  {workoutRatingMap[workout.rating].span}
+                                </span>
+                              </div>
+                            )}
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
+                  </ScrollShadow>
+                ) : (
+                  <div className="flex justify-center text-stone-500 font-medium">
+                    No Workouts Completed
                   </div>
-                </ScrollShadow>
+                )}
               </div>
             </ModalBody>
             <ModalFooter>
