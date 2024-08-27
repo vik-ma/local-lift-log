2c3d018e26f781d0a0cc2163dfa47a3d46d6309d
Viktor <vikt.mag@gmail.com>
2024-08-27 14:04:19 +0200

    Add isTemplate prop to GetUniqueMultisetIds helper function and add query string for workout_templates table

--- src/helpers/Workouts/GetUniqueMultisetIds.tsx
+++ src/helpers/Workouts/GetUniqueMultisetIds.tsx
@@ -4,15 +4,23 @@ type DistinctMultisetIdQuery = {
   multiset_id: number;
 };
 
-export const GetUniqueMultisetIds = async (id: number): Promise<number[]> => {
+export const GetUniqueMultisetIds = async (
+  id: number,
+  isTemplate: boolean
+): Promise<number[]> => {
   try {
     const db = await Database.load(import.meta.env.VITE_DB);
 
-    const result = await db.select<DistinctMultisetIdQuery[]>(
-      `SELECT DISTINCT multiset_id FROM sets
-      WHERE workout_id = $1 AND multiset_id != 0`,
-      [id]
-    );
+    const queryString = isTemplate
+      ? `SELECT DISTINCT multiset_id FROM sets
+      WHERE workout_template_id = $1 
+      AND is_template = 1 AND multiset_id != 0`
+      : `SELECT DISTINCT multiset_id FROM sets
+      WHERE workout_id = $1 AND multiset_id != 0`;
+
+    const result = await db.select<DistinctMultisetIdQuery[]>(queryString, [
+      id,
+    ]);
 
     const multisetIds: number[] = result.map((multisetId) => {
       return multisetId.multiset_id;
