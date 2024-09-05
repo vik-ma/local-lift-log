a250577bd7ea917ecf80ae59f08e5d88f2600e19
Viktor <vikt.mag@gmail.com>
2024-09-05 15:30:19 +0200

    Add extraContent prop to EmptyListLabel component

--- src/components/EmptyListLabel.tsx
+++ src/components/EmptyListLabel.tsx
@@ -1,11 +1,18 @@
+import { ReactNode } from "react";
+
 type EmptyListLabelProps = {
   itemName: string;
+  extraContent?: ReactNode;
 };
 
-export const EmptyListLabel = ({ itemName }: EmptyListLabelProps) => {
+export const EmptyListLabel = ({
+  itemName,
+  extraContent,
+}: EmptyListLabelProps) => {
   return (
     <div className="flex flex-col items-center justify-center text-stone-500 py-2">
       <h2>No {itemName} Found</h2>
+      {extraContent !== undefined && extraContent}
     </div>
   );
 };
