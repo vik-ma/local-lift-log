8a9e4cd68c5b0dc3bd744830f5654c39a7e006e3
Viktor <vikt.mag@gmail.com>
2024-09-09 15:06:50 +0200

    Fix EmptyListLabel not showing customLabel

--- src/components/EmptyListLabel.tsx
+++ src/components/EmptyListLabel.tsx
@@ -14,7 +14,7 @@ export const EmptyListLabel = ({
   return (
     <div className="flex flex-col items-center justify-center text-stone-500 py-2">
       <h2>
-        {customLabel !== undefined ? `No ${itemName} Found` : customLabel}
+        {customLabel !== undefined ? customLabel : `No ${itemName} Found`}
       </h2>
       {extraContent !== undefined && extraContent}
     </div>
