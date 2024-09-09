752324239715fe3ed028d057f489989b4d9cebe7
Viktor <vikt.mag@gmail.com>
2024-09-09 15:09:42 +0200

    Add className prop to EmptyListLabel

--- src/components/EmptyListLabel.tsx
+++ src/components/EmptyListLabel.tsx
@@ -4,16 +4,18 @@ type EmptyListLabelProps = {
   itemName: string;
   customLabel?: string;
   extraContent?: ReactNode;
+  className?: string;
 };
 
 export const EmptyListLabel = ({
   itemName,
   customLabel,
   extraContent,
+  className,
 }: EmptyListLabelProps) => {
   return (
     <div className="flex flex-col items-center justify-center text-stone-500 py-2">
-      <h2>
+      <h2 className={className !== undefined ? className : ""}>
         {customLabel !== undefined ? customLabel : `No ${itemName} Found`}
       </h2>
       {extraContent !== undefined && extraContent}
