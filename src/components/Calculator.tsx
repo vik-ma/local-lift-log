c79b9ce22037fefe39689c048167eba2655b17fd
Viktor <vikt.mag@gmail.com>
2024-09-11 20:56:19 +0200

    Add styling to buttons in Calculator component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,26 +1,26 @@
 export const Calculator = () => {
   return (
-    <div className="grid grid-rows-5 grid-cols-4">
-      <button className="">(</button>
-      <button className="">)</button>
-      <button className="">C</button>
-      <button className="">/</button>
-      <button className="">7</button>
-      <button className="">8</button>
-      <button className="">9</button>
-      <button className="">x</button>
-      <button className="">4</button>
-      <button className="">5</button>
-      <button className="">6</button>
-      <button className="">-</button>
-      <button className="">1</button>
-      <button className="">2</button>
-      <button className="">3</button>
-      <button className="">+</button>
-      <button className="">.</button>
-      <button className="">0</button>
-      <button className="">Undo</button>
-      <button className="">=</button>
+    <div className="grid grid-rows-5 grid-cols-4 gap-0.5 px-10">
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">(</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">)</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">C</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">/</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">7</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">8</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">9</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">x</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">4</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">5</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">6</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">-</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">1</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">2</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">3</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">+</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">.</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">0</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">Undo</button>
+      <button className="h-12 text-default-500 text-xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">=</button>
     </div>
   );
 };
