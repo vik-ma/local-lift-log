8202dcf722855d6b120729a8f507e711a97ceb9e
Viktor <vikt.mag@gmail.com>
2024-09-12 14:21:54 +0200

    Tweak color of icons in Calculator buttons

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -20,55 +20,55 @@ export const Calculator = () => {
         C
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <DivideIcon size={26} />
+        <DivideIcon size={26} color="#848484" />
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         7
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         8
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         9
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <CrossIcon size={24} color="#999" />
+        <CrossIcon size={24} color="#848484" />
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         4
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         5
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         6
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <MinusIcon size={36} />
+        <MinusIcon size={36} color="#848484" />
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         1
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         2
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         3
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <PlusIcon size={36} />
+        <PlusIcon size={36} color="#848484" />
       </button>
-      <button className="flex pt-0.5  justify-center items-center h-12 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="flex pt-0.5 justify-center items-center h-12 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         .
       </button>
-      <button className="h-12 pt-0.5  text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
+      <button className="h-12 pt-0.5 text-default-500 text-2xl font-medium border-2 border-default-300 rounded-lg hover:bg-default-100">
         0
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <BackspaceIcon size={32} color="#999" />
+        <BackspaceIcon size={28} color="#848484" />
       </button>
       <button className="flex justify-center items-center h-12 border-2 border-default-300 rounded-lg bg-default-100 hover:bg-default-200">
-        <EqualsIcon size={36} />
+        <EqualsIcon size={36} color="#848484" />
       </button>
     </div>
   );
