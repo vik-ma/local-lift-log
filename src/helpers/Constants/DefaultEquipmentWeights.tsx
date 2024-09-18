35803eeaec0ae8c6d6f68a01f7b9b3446e5148b6
Viktor <vikt.mag@gmail.com>
2024-09-18 13:03:24 +0200

    Add common olympic plate weights to DefaultEquipmentWeights list

--- src/helpers/Constants/DefaultEquipmentWeights.tsx
+++ src/helpers/Constants/DefaultEquipmentWeights.tsx
@@ -23,6 +23,31 @@ export const DefaultEquipmentWeights = (
     },
   ];
 
+  const metricWeights = [1.25, 2.5, 5, 10, 15, 20, 25];
+  const imperialWeights = [2.5, 5, 10, 25, 35, 45];
+
+  if (isMetric) {
+    for (const weight of metricWeights) {
+      const equipmentWeight: DefaultEquipmentWeight = {
+        name: `${weight} kg`,
+        weight: weight,
+        weight_unit: "kg",
+        is_favorite: 0,
+      };
+      DEFAULT_EQUIPMENT_WEIGHTS.push(equipmentWeight);
+    }
+  } else {
+    for (const weight of imperialWeights) {
+      const equipmentWeight: DefaultEquipmentWeight = {
+        name: `${weight} lbs`,
+        weight: weight,
+        weight_unit: "lbs",
+        is_favorite: 0,
+      };
+      DEFAULT_EQUIPMENT_WEIGHTS.push(equipmentWeight);
+    }
+  }
+
   Object.freeze(DEFAULT_EQUIPMENT_WEIGHTS);
 
   return DEFAULT_EQUIPMENT_WEIGHTS;
