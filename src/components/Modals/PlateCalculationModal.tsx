41c987d449432752f9dae302d0c6c15d4a79f54a
Viktor <vikt.mag@gmail.com>
2024-10-19 21:07:01 +0200

    Add button to change page in PlateCalculationModal

--- src/components/Modals/PlateCalculationModal.tsx
+++ src/components/Modals/PlateCalculationModal.tsx
@@ -28,6 +28,14 @@ export const PlateCalculationModal = ({
 }: PlateCalculationModalProps) => {
   const isNameInputValid = useValidateName(plateCalculation.name);
 
+  const changePlateCalculatorPage = () => {
+    if (plateCalculationModal.plateCalculatorPage === "base") {
+      plateCalculationModal.setPlateCalculatorPage("equipment-list");
+    } else {
+      plateCalculationModal.setPlateCalculatorPage("base");
+    }
+  };
+
   return (
     <Modal
       isOpen={plateCalculationModal.plateCalculationModal.isOpen}
@@ -40,35 +48,56 @@ export const PlateCalculationModal = ({
               {plateCalculation.id === 0 ? "New" : "Edit"} Plate Calculation
             </ModalHeader>
             <ModalBody>
-              <div className="flex flex-col gap-5">
-                <div className="flex flex-col gap-0.5">
-                  <Input
-                    className="h-[5rem]"
-                    value={plateCalculation.name}
-                    isInvalid={!isNameInputValid}
-                    label="Name"
-                    errorMessage={!isNameInputValid && "Name can't be empty"}
-                    variant="faded"
-                    onValueChange={(value) =>
-                      setPlateCalculation((prev) => ({ ...prev, name: value }))
-                    }
-                    isRequired
-                    isClearable
-                  />
+              {plateCalculationModal.plateCalculatorPage === "base" ? (
+                <div className="flex flex-col gap-5">
+                  <div className="flex flex-col gap-0.5">
+                    <Input
+                      className="h-[5rem]"
+                      value={plateCalculation.name}
+                      isInvalid={!isNameInputValid}
+                      label="Name"
+                      errorMessage={!isNameInputValid && "Name can't be empty"}
+                      variant="faded"
+                      onValueChange={(value) =>
+                        setPlateCalculation((prev) => ({
+                          ...prev,
+                          name: value,
+                        }))
+                      }
+                      isRequired
+                      isClearable
+                    />
+                  </div>
                 </div>
-              </div>
+              ) : (
+                <div>test</div>
+              )}
             </ModalBody>
-            <ModalFooter>
-              <Button color="primary" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button
-                color="primary"
-                onPress={buttonAction}
-                isDisabled={!isNameInputValid}
-              >
-                {plateCalculation.id !== 0 ? "Save" : "Create"}
-              </Button>
+            <ModalFooter className="flex justify-between">
+              <div className="flex gap-2">
+                <Button
+                  className="w-[7rem]"
+                  color="secondary"
+                  variant="flat"
+                  onPress={changePlateCalculatorPage}
+                >
+                  {plateCalculationModal.plateCalculatorPage === "base"
+                    ? "Add Weight"
+                    : "Back"}
+                </Button>
+              </div>
+              <div className="flex gap-2">
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color="primary"
+                  onPress={buttonAction}
+                  isDisabled={!isNameInputValid}
+                >
+                  {plateCalculation.id !== 0 ? "Save" : "Create"}
+                </Button>
+              </div>
             </ModalFooter>
           </>
         )}
