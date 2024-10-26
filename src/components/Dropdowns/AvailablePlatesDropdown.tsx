278417434bd090e2f4e7cecf41fb18bf4e303a45
Viktor <vikt.mag@gmail.com>
2024-10-26 14:58:19 +0200

    Add handleChange function to AvailablePlatesDropdown and add AvailablePlatesDropdown to PresetsModalList

--- src/components/Dropdowns/AvailablePlatesDropdown.tsx
+++ src/components/Dropdowns/AvailablePlatesDropdown.tsx
@@ -1,21 +1,69 @@
 import { Select, SelectItem } from "@nextui-org/react";
+import { useMemo } from "react";
+import { EquipmentWeight, PlateCalculation } from "../../typings";
+import { GenerateFormattedAvailablePlatesString } from "../../helpers";
 
 type AvailablePlatesDropdownProps = {
   value: number;
+  equipmentWeight: EquipmentWeight;
+  operatingPlateCalculation: PlateCalculation;
+  setOperatingPlateCalculation: React.Dispatch<
+    React.SetStateAction<PlateCalculation>
+  >;
 };
 
 export const AvailablePlatesDropdown = ({
   value,
+  equipmentWeight,
+  operatingPlateCalculation,
+  setOperatingPlateCalculation,
 }: AvailablePlatesDropdownProps) => {
-  const availableNumbers = [2, 4, 6, 8, 10, 12, 16, 18, 20];
+  const availableNumbers = useMemo(() => {
+    return ["2", "4", "6", "8", "10", "12", "16", "18", "20"];
+  }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {};
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (
+      operatingPlateCalculation.availablePlatesMap === undefined ||
+      !operatingPlateCalculation.availablePlatesMap.has(equipmentWeight)
+    )
+      return;
+
+    const value = e.target.value;
+
+    if (!availableNumbers.includes(value)) return;
+
+    const numValue = Number(value);
+
+    const updatedAvailablePlatesMap = new Map(
+      operatingPlateCalculation.availablePlatesMap
+    );
+
+    updatedAvailablePlatesMap.set(equipmentWeight, numValue);
+
+    const {
+      available_plates_string,
+      formattedAvailablePlatesString,
+      formattedAvailablePlatesMapString,
+    } = GenerateFormattedAvailablePlatesString(updatedAvailablePlatesMap);
+
+    const updatedPlateCalculation = {
+      ...operatingPlateCalculation,
+      available_plates_string,
+      availablePlatesMap: updatedAvailablePlatesMap,
+      formattedAvailablePlatesString,
+      formattedAvailablePlatesMapString,
+    };
+
+    setOperatingPlateCalculation(updatedPlateCalculation);
+  };
 
   return (
     <Select
       aria-label="Available Plates Dropdown List"
+      className="w-[4.5rem]"
       variant="faded"
-      selectedKeys={[value]}
+      selectedKeys={[value.toString()]}
       onChange={(e) => handleChange(e)}
       disallowEmptySelection
     >
--- src/components/PresetsModalList.tsx
+++ src/components/PresetsModalList.tsx
@@ -1,5 +1,6 @@
 import { Button, ScrollShadow } from "@nextui-org/react";
 import {
+  AvailablePlatesDropdown,
   EmptyListLabel,
   FavoriteButton,
   LoadingSpinner,
@@ -51,6 +52,7 @@ export const PresetsModalList = ({
     sortCategoryEquipment,
     handleSortOptionSelectionEquipment,
     operatingPlateCalculation,
+    setOperatingPlateCalculation,
     updateAvailablePlatesMapKeys,
   } = presetsList;
 
@@ -126,72 +128,94 @@ export const PresetsModalList = ({
             <LoadingSpinner />
           ) : (
             <>
-              {filteredEquipmentWeights.map((equipment) => (
-                <div
-                  className={
-                    validWeightUnit !== undefined &&
-                    equipment.weight_unit !== validWeightUnit &&
-                    hideInvalidUnitItems
-                      ? "hidden"
-                      : validWeightUnit !== undefined &&
-                        equipment.weight_unit !== validWeightUnit
-                      ? "flex justify-between items-center gap-1 opacity-40 cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
-                      : showPlateCalculatorButton
-                      ? "flex justify-between items-center gap-1 bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
-                      : "flex justify-between items-center gap-1 cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
-                  }
-                  key={`equipment-${equipment.id}`}
-                  onClick={() => handlePresetClick(equipment, undefined)}
-                >
-                  <div className="flex flex-col justify-start items-start pl-2 py-1">
-                    <span className="w-[16.25rem] truncate text-left">
-                      {equipment.name}
-                    </span>
-                    <span className="text-xs text-secondary text-left">
-                      {equipment.weight} {equipment.weight_unit}
-                    </span>
-                  </div>
-                  <div className="flex gap-1 items-center pr-2">
-                    {showPlateCalculatorButton && (
-                      <Button
-                        aria-label={
-                          operatingPlateCalculation?.availablePlatesMap?.has(
-                            equipment
-                          )
-                            ? `Remove ${equipment.name} From Plate Calculation`
-                            : `Add ${equipment.name} To Plate Calculation`
+              {filteredEquipmentWeights.map((equipment) => {
+                const isInPlateCalculation =
+                  operatingPlateCalculation?.availablePlatesMap?.has(
+                    equipment
+                  ) ?? false;
+
+                const numAvailable =
+                  operatingPlateCalculation?.availablePlatesMap?.get(
+                    equipment
+                  ) ?? 0;
+
+                return (
+                  <div
+                    className={
+                      validWeightUnit !== undefined &&
+                      equipment.weight_unit !== validWeightUnit &&
+                      hideInvalidUnitItems
+                        ? "hidden"
+                        : validWeightUnit !== undefined &&
+                          equipment.weight_unit !== validWeightUnit
+                        ? "flex justify-between items-center gap-1 opacity-40 cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
+                        : showPlateCalculatorButton
+                        ? "flex justify-between items-center gap-1 bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
+                        : "flex justify-between items-center gap-1 cursor-pointer bg-default-100 border-2 border-default-200 rounded-xl hover:border-default-400 focus:bg-default-200 focus:border-default-400"
+                    }
+                    key={`equipment-${equipment.id}`}
+                    onClick={() => handlePresetClick(equipment, undefined)}
+                  >
+                    <div className="flex flex-col justify-start items-start pl-2 py-1">
+                      <span
+                        className={
+                          isInPlateCalculation
+                            ? "w-[11.5rem] truncate text-left"
+                            : "w-[16.25rem] truncate text-left"
                         }
-                        isIconOnly
-                        className="z-1 w-[3.5rem]"
-                        color={
-                          operatingPlateCalculation?.availablePlatesMap?.has(
-                            equipment
-                          )
-                            ? "success"
-                            : "default"
-                        }
-                        variant="light"
-                        onPress={() => updateAvailablePlatesMapKeys(equipment)}
                       >
-                        <WeightPlatesIcon
-                          isChecked={
-                            operatingPlateCalculation?.availablePlatesMap?.has(
-                              equipment
-                            ) ?? false
-                          }
-                          size={31}
-                        />
-                      </Button>
-                    )}
-                    <FavoriteButton
-                      name={equipment.name}
-                      isFavorite={!!equipment.is_favorite}
-                      item={equipment}
-                      toggleFavorite={toggleFavoriteEquipmentWeight}
-                    />
+                        {equipment.name}
+                      </span>
+                      <span className="text-xs text-secondary text-left">
+                        {equipment.weight} {equipment.weight_unit}
+                      </span>
+                    </div>
+                    <div className="flex gap-1 items-center pr-2">
+                      {showPlateCalculatorButton && (
+                        <>
+                          {numAvailable !== 0 && (
+                            <AvailablePlatesDropdown
+                              value={numAvailable}
+                              equipmentWeight={equipment}
+                              operatingPlateCalculation={
+                                operatingPlateCalculation
+                              }
+                              setOperatingPlateCalculation={
+                                setOperatingPlateCalculation
+                              }
+                            />
+                          )}
+                          <Button
+                            aria-label={
+                              isInPlateCalculation
+                                ? `Remove ${equipment.name} From Plate Calculation`
+                                : `Add ${equipment.name} To Plate Calculation`
+                            }
+                            isIconOnly
+                            className="z-1 w-[3.5rem]"
+                            color={isInPlateCalculation ? "success" : "default"}
+                            variant="light"
+                            onPress={() =>
+                              updateAvailablePlatesMapKeys(equipment)
+                            }
+                          >
+                            <WeightPlatesIcon
+                              isChecked={isInPlateCalculation}
+                              size={31}
+                            />
+                          </Button>
+                        </>
+                      )}
+                      <FavoriteButton
+                        name={equipment.name}
+                        isFavorite={!!equipment.is_favorite}
+                        item={equipment}
+                        toggleFavorite={toggleFavoriteEquipmentWeight}
+                      />
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
               {filteredEquipmentWeights.length === 0 && (
                 <EmptyListLabel itemName="Equipment Weights" />
               )}
