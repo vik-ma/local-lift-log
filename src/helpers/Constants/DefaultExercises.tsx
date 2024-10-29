f271809a03467b00828532fe220d86f71da34603
Viktor <vikt.mag@gmail.com>
2024-10-29 11:26:24 +0100

    Update DefaultExercises with separate primary and secondary exercise_group_set_string props

--- src/helpers/Constants/DefaultExercises.tsx
+++ src/helpers/Constants/DefaultExercises.tsx
@@ -1,49 +1,62 @@
 type DefaultExercise = {
   name: string;
-  exercise_group_set_string: string;
+  exercise_group_set_string_primary: string;
+  exercise_group_set_string_secondary: string;
 };
 
 export const DefaultExercises = (): DefaultExercise[] => {
+  const defaultMultiplier = 0.5;
+
   const DEFAULT_EXERCISES: DefaultExercise[] = [
     {
       name: "Bench Press",
-      exercise_group_set_string: "0,1",
+      exercise_group_set_string_primary: "0",
+      exercise_group_set_string_secondary: `1x${defaultMultiplier},4x${defaultMultiplier}`,
     },
     {
       name: "Hammer Curl",
-      exercise_group_set_string: "2,3",
+      exercise_group_set_string_primary: "2,3",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Lateral Raise",
-      exercise_group_set_string: "4",
+      exercise_group_set_string_primary: "4",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Deadlift",
-      exercise_group_set_string: "5,6,7,8,9,10,11",
+      exercise_group_set_string_primary: "7,8",
+      exercise_group_set_string_secondary: `3x${defaultMultiplier},5x${defaultMultiplier},6x${defaultMultiplier},9x${defaultMultiplier},10x${defaultMultiplier},11x${defaultMultiplier}`,
     },
     {
       name: "Calf Raise",
-      exercise_group_set_string: "12",
+      exercise_group_set_string_primary: "12",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Sit-Up",
-      exercise_group_set_string: "13",
+      exercise_group_set_string_primary: "13",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Crush Gripper",
-      exercise_group_set_string: "14",
+      exercise_group_set_string_primary: "14",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Neck Curl",
-      exercise_group_set_string: "15",
+      exercise_group_set_string_primary: "15",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Stationary Bike",
-      exercise_group_set_string: "16",
+      exercise_group_set_string_primary: "16",
+      exercise_group_set_string_secondary: "",
     },
     {
       name: "Other",
-      exercise_group_set_string: "17",
+      exercise_group_set_string_primary: "17",
+      exercise_group_set_string_secondary: "",
     },
   ];
 
