f81a841f41eef6682afa446d817a22dcc86c7476
Viktor <vikt.mag@gmail.com>
2024-10-31 04:45:55 +0100

    Modify ConvertExerciseGroupStringMapSecondaryToString to add new items from list

--- src/helpers/Exercises/ConvertExerciseGroupStringMapSecondaryToString.tsx
+++ src/helpers/Exercises/ConvertExerciseGroupStringMapSecondaryToString.tsx
@@ -1,14 +1,21 @@
 export const ConvertExerciseGroupStringMapSecondaryToString = (
+  exerciseGroupStringListSecondary: string[],
   exerciseGroupStringMap: Map<string, string>
 ): string | null => {
   if (exerciseGroupStringMap.size === 0) return null;
 
   const exerciseGroupStrings: string[] = [];
 
-  for (const [group, multiplier] of exerciseGroupStringMap) {
-    const exerciseGroupString = `${group}x${multiplier}`;
-
-    exerciseGroupStrings.push(exerciseGroupString);
+  for (const group of exerciseGroupStringListSecondary) {
+    if (exerciseGroupStringMap.has(group)) {
+      const multiplier = exerciseGroupStringMap.get(group);
+      const exerciseGroupString = `${group}x${multiplier}`;
+      exerciseGroupStrings.push(exerciseGroupString);
+    } else {
+      // Add new Exercise Group
+      const newExerciseGroupString = `${group}x0.5`;
+      exerciseGroupStrings.push(newExerciseGroupString);
+    }
   }
 
   const exerciseGroupString = exerciseGroupStrings.join(",");
