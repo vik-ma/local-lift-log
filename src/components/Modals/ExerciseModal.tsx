da20728e89b4592fc4d9c1ba9fffe67672b1eba1
Viktor <vikt.mag@gmail.com>
2024-10-31 02:59:22 +0100

    Add ScrollShadow to ExerciseModal ModalBody

--- src/components/Modals/ExerciseModal.tsx
+++ src/components/Modals/ExerciseModal.tsx
@@ -76,67 +76,69 @@ export const ExerciseModal = ({
               {exercise.id === 0 ? "New" : "Edit"} Exercise
             </ModalHeader>
             <ModalBody>
-              <div className="flex flex-col gap-0.5">
-                <Input
-                  className="h-[5rem]"
-                  value={exercise.name}
-                  isInvalid={!isExerciseNameValid}
-                  label="Name"
-                  errorMessage={!isExerciseNameValid && "Name can't be empty"}
-                  variant="faded"
-                  onValueChange={(value) =>
-                    setExercise((prev) => ({ ...prev, name: value }))
-                  }
-                  isRequired
-                  isClearable
-                />
-                <Input
-                  value={exercise.note ?? ""}
-                  label="Note"
-                  variant="faded"
-                  onValueChange={(value) =>
-                    setExercise((prev) => ({ ...prev, note: value }))
-                  }
-                  isClearable
-                />
-              </div>
-              <div
-                aria-label="Primary Exercise Groups Accordion"
-                className="flex flex-col select-none cursor-pointer"
-                onClick={() =>
-                  setIsPrimaryAccordionExpanded(!isPrimaryAccordionExpanded)
-                }
-              >
-                <div className="flex justify-between items-center pl-1 pb-1">
-                  <span className="font-medium">Primary Exercise Groups</span>
-                  <ChevronIcon
-                    size={25}
-                    color="#a8a29e"
-                    direction={isPrimaryAccordionExpanded ? "down" : "left"}
+              <ScrollShadow className="flex flex-col gap-2.5 h-[440px]">
+                <div className="flex flex-col gap-0.5">
+                  <Input
+                    className="h-[5rem]"
+                    value={exercise.name}
+                    isInvalid={!isExerciseNameValid}
+                    label="Name"
+                    errorMessage={!isExerciseNameValid && "Name can't be empty"}
+                    variant="faded"
+                    onValueChange={(value) =>
+                      setExercise((prev) => ({ ...prev, name: value }))
+                    }
+                    isRequired
+                    isClearable
+                  />
+                  <Input
+                    value={exercise.note ?? ""}
+                    label="Note"
+                    variant="faded"
+                    onValueChange={(value) =>
+                      setExercise((prev) => ({ ...prev, note: value }))
+                    }
+                    isClearable
                   />
                 </div>
-                <AnimatePresence>
-                  {isPrimaryAccordionExpanded && (
-                    <motion.div
-                      className="px-1"
-                      initial={{ height: 0 }}
-                      animate={{ height: "auto" }}
-                      exit={{ height: 0, opacity: 0 }}
-                      transition={{
-                        height: { duration: 0.1 },
-                        opacity: { duration: 0.05 },
-                      }}
-                    >
-                      <ExerciseGroupCheckboxes
-                        isValid={isExerciseGroupSetStringValid}
-                        value={exercise.exerciseGroupStringListPrimary ?? []}
-                        handleChange={handleExerciseGroupStringPrimaryChange}
-                        exerciseGroupList={exerciseGroupList}
-                      />
-                    </motion.div>
-                  )}
-                </AnimatePresence>
-              </div>
+                <div
+                  aria-label="Primary Exercise Groups Accordion"
+                  className="flex flex-col select-none cursor-pointer"
+                  onClick={() =>
+                    setIsPrimaryAccordionExpanded(!isPrimaryAccordionExpanded)
+                  }
+                >
+                  <div className="flex justify-between items-center pl-1 pb-1">
+                    <span className="font-medium">Primary Exercise Groups</span>
+                    <ChevronIcon
+                      size={25}
+                      color="#a8a29e"
+                      direction={isPrimaryAccordionExpanded ? "down" : "left"}
+                    />
+                  </div>
+                  <AnimatePresence>
+                    {isPrimaryAccordionExpanded && (
+                      <motion.div
+                        className="px-1"
+                        initial={{ height: 0 }}
+                        animate={{ height: "auto" }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{
+                          height: { duration: 0.1 },
+                          opacity: { duration: 0.05 },
+                        }}
+                      >
+                        <ExerciseGroupCheckboxes
+                          isValid={isExerciseGroupSetStringValid}
+                          value={exercise.exerciseGroupStringListPrimary ?? []}
+                          handleChange={handleExerciseGroupStringPrimaryChange}
+                          exerciseGroupList={exerciseGroupList}
+                        />
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </ScrollShadow>
             </ModalBody>
             <ModalFooter>
               <Button color="primary" variant="light" onPress={onClose}>
