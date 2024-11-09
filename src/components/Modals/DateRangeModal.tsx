c931f2a7703c03ac4fa2d69798fa8392c6db24fd
Viktor <vikt.mag@gmail.com>
2024-11-09 18:38:12 +0100

    Add DateRangePicker and locale prop to DateRangeModal

--- src/components/Modals/DateRangeModal.tsx
+++ src/components/Modals/DateRangeModal.tsx
@@ -5,18 +5,31 @@ import {
   ModalBody,
   ModalHeader,
   ModalFooter,
+  DateRangePicker,
+  RangeValue,
+  CalendarDate,
 } from "@nextui-org/react";
 import { UseDisclosureReturnType } from "../../typings";
+import { I18nProvider } from "@react-aria/i18n";
+import { useState } from "react";
 
 type DateRangeModalProps = {
   dateRangeModal: UseDisclosureReturnType;
+  header: string;
+  locale: string;
   buttonAction: () => void;
 };
 
 export const DateRangeModal = ({
   dateRangeModal,
+  header,
+  locale,
   buttonAction,
 }: DateRangeModalProps) => {
+  const [dateRange, setDateRange] = useState<RangeValue<CalendarDate> | null>(
+    null
+  );
+
   return (
     <Modal
       isOpen={dateRangeModal.isOpen}
@@ -25,9 +38,19 @@ export const DateRangeModal = ({
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader>Select Date Range</ModalHeader>
+            <ModalHeader>{header}</ModalHeader>
             <ModalBody>
-              <div className="h-16"></div>
+              <div className="h-16">
+                <I18nProvider locale={locale}>
+                  <DateRangePicker
+                    label="Stay duration"
+                    variant="faded"
+                    value={dateRange}
+                    onChange={setDateRange}
+                    visibleMonths={2}
+                  />
+                </I18nProvider>
+              </div>
             </ModalBody>
             <ModalFooter>
               <Button color="primary" variant="light" onPress={onClose}>
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -102,7 +102,12 @@ export default function Test() {
         userSettings={userSettings}
         setUserSettings={setUserSettings}
       />
-      <DateRangeModal dateRangeModal={dateRangeModal} buttonAction={() => {}} />
+      <DateRangeModal
+        header="Select Date Range"
+        dateRangeModal={dateRangeModal}
+        locale={userSettings.locale}
+        buttonAction={() => {}}
+      />
       <div className="flex flex-col gap-2">
         <div className="flex justify-center bg-neutral-900 px-6 py-4 rounded-xl">
           <h1 className="tracking-tight inline font-bold from-[#FF705B] to-[#FFB457] text-6xl bg-clip-text text-transparent bg-gradient-to-b truncate">
