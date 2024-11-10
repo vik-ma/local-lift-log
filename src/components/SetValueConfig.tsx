8398797d6b0f19b0d468e3f2c8619bd258c1f4cd
Viktor <vikt.mag@gmail.com>
2024-11-10 12:21:42 +0100

    Change styling of SetValueConfig

--- src/components/SetValueConfig.tsx
+++ src/components/SetValueConfig.tsx
@@ -100,236 +100,260 @@ export const SetValueConfig = ({
           )}
         </h2>
       </div>
-      <ScrollShadow className="flex flex-col gap-2 h-full">
-        {showNoteInput && (
-          <Input
-            value={operatingSet.note ?? ""}
-            label="Note"
-            variant="faded"
-            size="sm"
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                note: value,
-              }))
-            }
-            isClearable
-          />
-        )}
-        <h3 className="text-xl font-semibold px-0.5">Track</h3>
-        <div className="grid grid-cols-2 gap-1.5 px-1">
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_weight ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_weight: value ? 1 : 0,
-              }))
-            }
-          >
-            Weight
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_reps ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_reps: value ? 1 : 0,
-              }))
-            }
-          >
-            Reps
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_distance ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_distance: value ? 1 : 0,
-              }))
-            }
-          >
-            Distance
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_time ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_time: value ? 1 : 0,
-              }))
-            }
-          >
-            Time
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_rir ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_rir: value ? 1 : 0,
-              }))
-            }
-          >
-            RIR
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_rpe ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_rpe: value ? 1 : 0,
-              }))
-            }
-          >
-            RPE
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={
-              operatingSet.is_tracking_resistance_level ? true : false
-            }
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_resistance_level: value ? 1 : 0,
-              }))
-            }
-          >
-            Resistance Level
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_partial_reps ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_partial_reps: value ? 1 : 0,
-              }))
-            }
-          >
-            Partial Reps
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_tracking_user_weight ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_tracking_user_weight: value ? 1 : 0,
-              }))
-            }
-          >
-            User Weight
-          </Checkbox>
-          <Checkbox
-            className="hover:underline"
-            color="primary"
-            isSelected={operatingSet.is_warmup ? true : false}
-            onValueChange={(value) =>
-              setOperatingSet((prev) => ({
-                ...prev,
-                is_warmup: value ? 1 : 0,
-              }))
-            }
-          >
-            <span className="text-yellow-500">Warmup Set</span>
-          </Checkbox>
-        </div>
-        {(operationType === "add" ||
-          operationType === "add-sets-to-multiset") &&
-          numNewSets &&
-          setNumNewSets && (
-            <div className="flex">
+      <ScrollShadow className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-[24rem]">
+          {showNoteInput && (
+            <Input
+              value={operatingSet.note ?? ""}
+              label="Note"
+              variant="faded"
+              size="sm"
+              onValueChange={(value) =>
+                setOperatingSet((prev) => ({
+                  ...prev,
+                  note: value,
+                }))
+              }
+              isClearable
+            />
+          )}
+          <h3 className="text-xl font-semibold px-0.5">Track</h3>
+          <div className="grid grid-cols-2 gap-1 px-1">
+            <div className="w-[11rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_weight ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_weight: value ? 1 : 0,
+                  }))
+                }
+              >
+                Weight
+              </Checkbox>
+            </div>
+            <div className="w-[9rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_reps ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_reps: value ? 1 : 0,
+                  }))
+                }
+              >
+                Reps
+              </Checkbox>
+            </div>
+            <div className="w-[11rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_distance ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_distance: value ? 1 : 0,
+                  }))
+                }
+              >
+                Distance
+              </Checkbox>
+            </div>
+            <div className="w-[9rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_time ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_time: value ? 1 : 0,
+                  }))
+                }
+              >
+                Time
+              </Checkbox>
+            </div>
+            <div className="w-[11rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_rir ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_rir: value ? 1 : 0,
+                  }))
+                }
+              >
+                RIR
+              </Checkbox>
+            </div>
+            <div className="w-[9rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_rpe ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_rpe: value ? 1 : 0,
+                  }))
+                }
+              >
+                RPE
+              </Checkbox>
+            </div>
+            <div className="w-[11rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={
+                  operatingSet.is_tracking_resistance_level ? true : false
+                }
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_resistance_level: value ? 1 : 0,
+                  }))
+                }
+              >
+                Resistance Level
+              </Checkbox>
+            </div>
+            <div className="w-[9rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={
+                  operatingSet.is_tracking_partial_reps ? true : false
+                }
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_partial_reps: value ? 1 : 0,
+                  }))
+                }
+              >
+                Partial Reps
+              </Checkbox>
+            </div>
+            <div className="w-[11rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_tracking_user_weight ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_tracking_user_weight: value ? 1 : 0,
+                  }))
+                }
+              >
+                User Weight
+              </Checkbox>
+            </div>
+            <div className="w-[9rem]">
+              <Checkbox
+                className="hover:underline w-full min-w-full"
+                color="primary"
+                isSelected={operatingSet.is_warmup ? true : false}
+                onValueChange={(value) =>
+                  setOperatingSet((prev) => ({
+                    ...prev,
+                    is_warmup: value ? 1 : 0,
+                  }))
+                }
+              >
+                <span className="text-yellow-500">Warmup Set</span>
+              </Checkbox>
+            </div>
+          </div>
+          {(operationType === "add" ||
+            operationType === "add-sets-to-multiset") &&
+            numNewSets &&
+            setNumNewSets && (
+              <div className="flex">
+                <Select
+                  label="Number Of Sets To Add"
+                  size="sm"
+                  variant="faded"
+                  selectedKeys={[numNewSets]}
+                  onChange={(e) => setNumNewSets(e.target.value)}
+                  disallowEmptySelection
+                >
+                  {numSetsOptions.map((num) => (
+                    <SelectItem key={num} value={num}>
+                      {num}
+                    </SelectItem>
+                  ))}
+                </Select>
+              </div>
+            )}
+          <div className="flex items-center px-0.5">
+            <h3 className="text-xl font-semibold">
+              {isSetCompleted ? "Completed Values" : "Default Values"}
+            </h3>
+            <div className="flex flex-grow gap-2 justify-end">
+              {showDefaultValues && isSetEdited && (
+                <Button
+                  variant="flat"
+                  size="sm"
+                  color="danger"
+                  onPress={() => resetSetInputValues(true)}
+                >
+                  Reset Values
+                </Button>
+              )}
+              <Button
+                variant="flat"
+                size="sm"
+                onPress={() => setShowDefaultValues(!showDefaultValues)}
+              >
+                {showDefaultValues ? "Hide" : "Show"}
+              </Button>
+            </div>
+          </div>
+          {showDefaultValues && (
+            <SetValueInputs
+              operatingSet={operatingSet}
+              setOperatingSet={setOperatingSet}
+              useSetTrackingInputs={useSetTrackingInputs}
+              userSettings={userSettings}
+              userWeight={userWeight}
+              userWeightModal={userWeightModal}
+              exercise={selectedExercise}
+              isActiveSet={false}
+              openCalculationModal={openCalculationModal}
+            />
+          )}
+          {operationType === "add-sets-to-multiset" &&
+            numMultisetSets &&
+            multisetSetTarget &&
+            setMultisetSetTarget && (
               <Select
-                label="Number Of Sets To Add"
+                label="Add To Multiset Set"
                 size="sm"
                 variant="faded"
-                selectedKeys={[numNewSets]}
-                onChange={(e) => setNumNewSets(e.target.value)}
+                selectedKeys={[multisetSetTarget]}
+                onChange={(e) => setMultisetSetTarget(e.target.value)}
                 disallowEmptySelection
               >
-                {numSetsOptions.map((num) => (
+                {Array.from({ length: numMultisetSets }, (_, i) =>
+                  (i + 1).toString()
+                ).map((num) => (
                   <SelectItem key={num} value={num}>
                     {num}
                   </SelectItem>
                 ))}
               </Select>
-            </div>
-          )}
-        <div className="flex items-center px-0.5">
-          <h3 className="text-xl font-semibold">
-            {isSetCompleted ? "Completed Values" : "Default Values"}
-          </h3>
-          <div className="flex flex-grow gap-2 justify-end">
-            {showDefaultValues && isSetEdited && (
-              <Button
-                variant="flat"
-                size="sm"
-                color="danger"
-                onPress={() => resetSetInputValues(true)}
-              >
-                Reset Values
-              </Button>
             )}
-            <Button
-              variant="flat"
-              size="sm"
-              onPress={() => setShowDefaultValues(!showDefaultValues)}
-            >
-              {showDefaultValues ? "Hide" : "Show"}
-            </Button>
-          </div>
         </div>
-        {showDefaultValues && (
-          <SetValueInputs
-            operatingSet={operatingSet}
-            setOperatingSet={setOperatingSet}
-            useSetTrackingInputs={useSetTrackingInputs}
-            userSettings={userSettings}
-            userWeight={userWeight}
-            userWeightModal={userWeightModal}
-            exercise={selectedExercise}
-            isActiveSet={false}
-            openCalculationModal={openCalculationModal}
-          />
-        )}
-        {operationType === "add-sets-to-multiset" &&
-          numMultisetSets &&
-          multisetSetTarget &&
-          setMultisetSetTarget && (
-            <Select
-              label="Add To Multiset Set"
-              size="sm"
-              variant="faded"
-              selectedKeys={[multisetSetTarget]}
-              onChange={(e) => setMultisetSetTarget(e.target.value)}
-              disallowEmptySelection
-            >
-              {Array.from({ length: numMultisetSets }, (_, i) =>
-                (i + 1).toString()
-              ).map((num) => (
-                <SelectItem key={num} value={num}>
-                  {num}
-                </SelectItem>
-              ))}
-            </Select>
-          )}
       </ScrollShadow>
     </div>
   );
