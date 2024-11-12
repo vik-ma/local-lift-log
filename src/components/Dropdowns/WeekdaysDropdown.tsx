d09022ed552981a86c310af5fe651820c20c1b55
Viktor <vikt.mag@gmail.com>
2024-11-12 12:57:10 +0100

    Make WeekdaysDropdown always sort the weekdays from Monday to Sunday

--- src/components/Dropdowns/WeekdaysDropdown.tsx
+++ src/components/Dropdowns/WeekdaysDropdown.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectItem, SharedSelection } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
 
 type WeekdaysDropdownProps = {
   values: Set<string>;
@@ -11,15 +11,26 @@ export const WeekdaysDropdown = ({
   setValues,
   weekdayMap,
 }: WeekdaysDropdownProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const weekdayKeys = e.target.value.split(",");
+    // Sort the keys 1-6 numerically, with 0 (Sunday) always last
+    weekdayKeys.sort((a, b) => {
+      if (a === "0") return 1;
+      if (b === "0") return -1;
+      return Number(a) - Number(b);
+    });
+
+    const updatedSet = new Set(weekdayKeys);
+    setValues(updatedSet);
+  };
+
   return (
     <Select
       selectionMode="multiple"
       label="Weekdays"
       variant="faded"
       selectedKeys={values}
-      onSelectionChange={
-        setValues as React.Dispatch<React.SetStateAction<SharedSelection>>
-      }
+      onChange={(e) => handleChange(e)}
       disallowEmptySelection
     >
       {Array.from(weekdayMap).map(([weekdayNum, weekdayLabel]) => (
