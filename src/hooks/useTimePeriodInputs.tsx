2539c456ed7db0662db655795a1dd8850f05d368
Viktor <vikt.mag@gmail.com>
2024-12-18 01:11:54 +0100

    Change start_date and end_date columns in time_periods to instead store ISO date strings

--- src/hooks/useTimePeriodInputs.tsx
+++ src/hooks/useTimePeriodInputs.tsx
@@ -1,7 +1,7 @@
 import { useMemo, useState } from "react";
 import { useValidateName } from ".";
 import { TimePeriod, UseTimePeriodInputsReturnType } from "../typings";
-import { CalendarDate } from "@internationalized/date";
+import { CalendarDate, getLocalTimeZone } from "@internationalized/date";
 import { IsEndDateBeforeStartDate, ParseDateString } from "../helpers";
 
 export const useTimePeriodInputs = (
@@ -36,13 +36,18 @@ export const useTimePeriodInputs = (
   const startDateString: string | null = useMemo(() => {
     if (startDate === null) return null;
 
-    return startDate.toString();
+    const startDateDate = startDate.toDate(getLocalTimeZone());
+
+    return startDateDate.toISOString();
   }, [startDate]);
 
   const endDateString: string | null = useMemo(() => {
     if (endDate === null) return null;
 
-    return endDate.toString();
+    const endDateDate = endDate.toDate(getLocalTimeZone());
+    endDateDate.setHours(23, 59, 59, 999);
+
+    return endDateDate.toISOString();
   }, [endDate]);
 
   return {
