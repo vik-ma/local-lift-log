51aa6b1899fce1de4e38f0313e1d7e6d949798bf
Viktor <vikt.mag@gmail.com>
2024-12-26 16:37:18 +0100

    Change colors of CaloricIntakeTypeSpan

--- src/components/CaloricIntakeTypeSpan.tsx
+++ src/components/CaloricIntakeTypeSpan.tsx
@@ -5,12 +5,14 @@ type CaloricIntakeTypeSpanProps = {
 export const CaloricIntakeTypeSpan = ({
   value,
 }: CaloricIntakeTypeSpanProps) => {
-  if (value === "Bulk") return <span className="text-xs text-lime-500">Bulk</span>;
+  if (value === "Bulk")
+    return <span className="text-xs text-violet-600">Bulk</span>;
 
-  if (value === "Cut") return <span className="text-xs text-red-500">Cut</span>;
+  if (value === "Cut")
+    return <span className="text-xs text-indigo-400">Cut</span>;
 
   if (value === "Maintenance")
-    return <span className="text-xs text-indigo-700">Maintenance</span>;
+    return <span className="text-xs text-slate-500">Maintenance</span>;
 
   return <></>;
 };
