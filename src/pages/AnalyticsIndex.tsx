ddae61f1aac35729240ffdb9e09363d76a44119b
Viktor <vikt.mag@gmail.com>
2025-01-29 11:31:03 +0100

    Add Chart to AnalyticsIndex

--- src/pages/AnalyticsIndex.tsx
+++ src/pages/AnalyticsIndex.tsx
@@ -22,6 +22,16 @@ import {
 } from "../components";
 import { UserSettings } from "../typings";
 import { GetUserSettings } from "../helpers";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { ChartConfig, ChartContainer } from "../components/ui/chart";
 
 type ListType = "exercise" | "measurement";
 
@@ -44,6 +54,53 @@ export default function AnalyticsIndex() {
   const dietLogList = useDietLogList(false);
 
   const { isDietLogListLoaded, getDietLogs } = dietLogList;
+
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+  const chartConfig: ChartConfig = {};
 
   useEffect(() => {
     const loadUserSettings = async () => {
@@ -120,6 +177,34 @@ export default function AnalyticsIndex() {
         useFilterExerciseList={filterExerciseList}
       />
       <div className="flex flex-col items-center gap-3">
+        <div className="bg-white pt-5 pb-1.5 rounded-xl">
+          <ChartContainer config={chartConfig} className="min-h-[450px]">
+            <LineChart width={0} height={0} data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis yAxisId="left" />
+              <YAxis yAxisId="right" orientation="right" />
+              <Tooltip />
+              <Legend />
+              <Line
+                yAxisId="left"
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                strokeWidth={2}
+                activeDot={{ r: 8 }}
+              />
+              <Line
+                yAxisId="right"
+                type="monotone"
+                dataKey="uv"
+                stroke="#82ca9d"
+                strokeWidth={2}
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ChartContainer>
+        </div>
         <div className="flex flex-col gap-1.5">
           <Button
             className="font-medium"
