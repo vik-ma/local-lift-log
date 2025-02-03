1cf6dcd6784b65119d497e653b803c6380e07a81
Viktor <vikt.mag@gmail.com>
2025-02-03 23:35:18 +0100

    Fix filteredChartData type error in AnalyticsIndex

--- src/pages/AnalyticsIndex.tsx
+++ src/pages/AnalyticsIndex.tsx
@@ -98,7 +98,7 @@ export default function AnalyticsIndex() {
             chartDataLines.includes(key as ChartDataCategory)
         )
       )
-    );
+    ) as ChartData;
   }, [chartData]);
 
   const listModal = useDisclosure();
