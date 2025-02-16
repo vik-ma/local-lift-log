cc4fa6d6a8f31a6a6b48f897d2562025f9afbc53
Viktor <vikt.mag@gmail.com>
2025-02-16 10:55:01 +0100

    Add Chip elements to show Date filters in AnalyticsIndex

--- src/pages/AnalyticsIndex.tsx
+++ src/pages/AnalyticsIndex.tsx
@@ -12,6 +12,7 @@ import {
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
+  Chip,
 } from "@heroui/react";
 import {
   useExerciseList,
@@ -1435,8 +1436,11 @@ export default function AnalyticsIndex() {
           )}
         </div>
         {isChartDataLoaded.current && (
-          <div className="bg-default-50 pt-4 pb-1.5 rounded-xl">
-            <ChartContainer config={chartConfig} className="w-[870px]">
+          <div className="flex gap-1.5">
+            <ChartContainer
+              config={chartConfig}
+              className="w-[800px] bg-default-50 pt-4 pb-1.5 rounded-xl"
+            >
               <ComposedChart
                 data={filteredChartData}
                 margin={{ top: 15, right: 5, left: 5 }}
@@ -1514,6 +1518,32 @@ export default function AnalyticsIndex() {
                 ))}
               </ComposedChart>
             </ChartContainer>
+            <div className="flex flex-col gap-1">
+              {filterMinDate !== null && (
+                <Chip
+                  radius="sm"
+                  color="secondary"
+                  variant="flat"
+                  onClose={() => setFilterMinDate(null)}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <span className="font-semibold">Min Date: </span>
+                  {FormatDateToShortString(filterMinDate, userSettings.locale)}
+                </Chip>
+              )}
+              {filterMaxDate !== null && (
+                <Chip
+                  radius="sm"
+                  color="secondary"
+                  variant="flat"
+                  onClose={() => setFilterMaxDate(null)}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <span className="font-semibold">Max Date: </span>
+                  {FormatDateToShortString(filterMaxDate, userSettings.locale)}
+                </Chip>
+              )}
+            </div>
           </div>
         )}
         <div className="flex flex-col gap-2 w-[960px]">
