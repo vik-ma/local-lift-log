3932889c458c2dcd2eb2de7643d29ead534c1791
Viktor <vikt.mag@gmail.com>
2025-03-02 03:40:42 +0100

    Add time_completed validation to GetCompletedSetsWithExerciseId SQL statement

--- src/helpers/Sets/GetCompletedSetsWithExerciseId.tsx
+++ src/helpers/Sets/GetCompletedSetsWithExerciseId.tsx
@@ -6,7 +6,14 @@ export const GetCompletedSetsWithExerciseId = async (exerciseId: number) => {
     const db = await Database.load(import.meta.env.VITE_DB);
 
     const result = await db.select<WorkoutSet[]>(
-      "SELECT * FROM sets WHERE exercise_id = $1 AND is_completed = 1",
+      `SELECT * FROM sets 
+       WHERE exercise_id = $1 
+        AND is_template = 0
+        AND is_completed = 1 
+        AND time_completed IS NOT NULL 
+        AND time_completed LIKE '____-__-__T__:__:__.___Z'
+        AND time_completed GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9][0-9][0-9]Z'
+       ORDER BY time_completed DESC`,
       [exerciseId]
     );
 
