03d0d4d529bc6690972a59bf1260bd3877ebdad0
Viktor <vikt.mag@gmail.com>
2025-03-02 03:14:27 +0100

    Create GetAnalyticsValuesForSetList helper function

---
+++ src/helpers/Sets/GetAnalyticsValuesForSetList.tsx
@@ -0,0 +1,64 @@
+import { ChartDataExerciseCategoryBase, WorkoutSet } from "../../typings";
+import { ConvertNumberToTwoDecimals } from "../Numbers/ConvertNumberToTwoDecimals";
+import { ConvertWeightValue } from "../Numbers/ConvertWeightValue";
+
+export const GetAnalyticsValuesForSetList = (
+  setList: WorkoutSet[],
+  loadExerciseOptions: Set<ChartDataExerciseCategoryBase>,
+  weightUnit: string,
+  distanceUnit: string
+) => {
+  const analyticsValues = new Map<ChartDataExerciseCategoryBase, number>();
+
+  let minWeight = Infinity;
+  let maxWeight = 0;
+  let totalWeight = 0;
+
+  for (const set of setList) {
+    if (set.is_tracking_weight) {
+      const weight = ConvertWeightValue(
+        set.weight,
+        set.weight_unit,
+        weightUnit
+      );
+
+      if (weight < minWeight) minWeight = weight;
+      if (weight > maxWeight) maxWeight = weight;
+
+      if (set.is_tracking_reps) {
+        totalWeight += weight * set.reps;
+      }
+    }
+  }
+
+  if (loadExerciseOptions.has("weight_min")) {
+    analyticsValues.set(
+      "weight_min",
+      minWeight === Infinity ? 0 : ConvertNumberToTwoDecimals(minWeight)
+    );
+  }
+
+  if (loadExerciseOptions.has("weight_max")) {
+    analyticsValues.set("weight_max", ConvertNumberToTwoDecimals(maxWeight));
+  }
+
+  if (loadExerciseOptions.has("weight_avg")) {
+    analyticsValues.set(
+      "weight_max",
+      ConvertNumberToTwoDecimals(totalWeight / setList.length)
+    );
+  }
+
+  if (loadExerciseOptions.has("weight_total")) {
+    analyticsValues.set(
+      "weight_total",
+      ConvertNumberToTwoDecimals(totalWeight)
+    );
+  }
+
+  if (loadExerciseOptions.has("num_sets")) {
+    analyticsValues.set("num_sets", setList.length);
+  }
+
+  return analyticsValues;
+};
--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -95,6 +95,7 @@ export { ShouldSetTrackingValueButtonBeDisabled } from "./Sets/ShouldSetTracking
 export { GetSetsOfLastCompletedExercise } from "./Sets/GetSetsOfLastCompletedExercise";
 export { CopySetTrackingValues } from "./Sets/CopySetTrackingValues";
 export { GetCompletedSetsWithExerciseId } from "./Sets/GetCompletedSetsWithExerciseId";
+export { GetAnalyticsValuesForSetList } from "./Sets/GetAnalyticsValuesForSetList";
 
 export { GenerateMultisetSetOrderString } from "./Multisets/GenerateMultisetSetOrderString";
 export { DefaultNewMultiset } from "./Multisets/DefaultNewMultiset";
