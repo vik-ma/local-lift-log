45a646bb1dbf2e7d47079af85f577d93467cdf51
Viktor <vikt.mag@gmail.com>
2025-03-03 15:06:38 +0100

    Make mergeChartData function in AnalyticsIndex fill in gaps of dates between list1 and list2

--- src/pages/AnalyticsIndex.tsx
+++ src/pages/AnalyticsIndex.tsx
@@ -554,7 +554,11 @@ export default function AnalyticsIndex() {
 
     const filledInChartData = fillInMissingDates(loadedChartData, locale);
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      locale
+    );
 
     setChartData(mergedChartData);
 
@@ -663,7 +667,11 @@ export default function AnalyticsIndex() {
 
     const filledInChartData = fillInMissingDates(loadedChartData, locale);
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      locale
+    );
 
     setChartData(mergedChartData);
 
@@ -1131,7 +1139,11 @@ export default function AnalyticsIndex() {
 
     const filledInChartData = fillInMissingDates(loadedChartData, locale);
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      locale
+    );
 
     setChartData(mergedChartData);
 
@@ -1222,7 +1234,11 @@ export default function AnalyticsIndex() {
 
     const filledInChartData = fillInMissingDates(loadedChartData, locale);
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      locale
+    );
 
     setChartData(mergedChartData);
 
@@ -1242,8 +1258,43 @@ export default function AnalyticsIndex() {
     if (!isChartDataLoaded.current) isChartDataLoaded.current = true;
   };
 
-  const mergeChartData = (list1: ChartDataItem[], list2: ChartDataItem[]) => {
+  const mergeChartData = (
+    list1: ChartDataItem[],
+    list2: ChartDataItem[],
+    locale: string
+  ) => {
     const chartDataDateMap = new Map<string, ChartDataItem>();
+
+    if (list1.length > 0 && list2.length > 0) {
+      const minDate1 = new Date(list1[0].date);
+      const maxDate1 = new Date(list1[list1.length - 1].date);
+      maxDate1.setDate(maxDate1.getDate() + 1);
+      const minDate2 = new Date(list2[0].date);
+      const maxDate2 = new Date(list2[list2.length - 1].date);
+      maxDate2.setDate(maxDate2.getDate() + 1);
+
+      const isGapAndList1ComesFirst = maxDate1.getTime() < minDate2.getTime();
+      const isGapAndList2ComesFirst = maxDate2.getTime() < minDate1.getTime();
+
+      if (isGapAndList1ComesFirst || isGapAndList2ComesFirst) {
+        const currentDate = isGapAndList1ComesFirst ? maxDate1 : maxDate2;
+        const endDate = isGapAndList1ComesFirst ? minDate2 : minDate1;
+
+        while (currentDate < endDate) {
+          const dateString = FormatDateToShortString(currentDate, locale);
+
+          const chartDataItem: ChartDataItem = { date: dateString };
+
+          if (isGapAndList1ComesFirst) {
+            list1.push(chartDataItem);
+          } else {
+            list2.push(chartDataItem);
+          }
+
+          currentDate.setDate(currentDate.getDate() + 1);
+        }
+      }
+    }
 
     // Add all props from both lists to ChartDataItem of same date
     const mergeIntoMap = (list: ChartDataItem[]) => {
@@ -1696,7 +1747,11 @@ export default function AnalyticsIndex() {
       userSettings.locale
     );
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      userSettings.locale
+    );
 
     setChartData(mergedChartData);
 
@@ -1819,13 +1874,16 @@ export default function AnalyticsIndex() {
 
     // TODO: ADD highestValues
 
-
     const filledInChartData = fillInMissingDates(
       loadedChartData,
       userSettings.locale
     );
 
-    const mergedChartData = mergeChartData(filledInChartData, chartData);
+    const mergedChartData = mergeChartData(
+      filledInChartData,
+      chartData,
+      userSettings.locale
+    );
 
     setChartData(mergedChartData);
 
