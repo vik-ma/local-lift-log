dce98d51df820dffda3f970c5d65233931cefbc3
Viktor <vikt.mag@gmail.com>
2025-03-07 12:33:34 +0100

    Make UpdateChartCommentMapForExercise helper function also update dataKeys for chartCommentMap ChartComment-list

--- src/helpers/Analytics/UpdateChartCommentMapForExercise.tsx
+++ src/helpers/Analytics/UpdateChartCommentMapForExercise.tsx
@@ -1,5 +1,6 @@
 import {
   ChartComment,
+  ChartDataCategory,
   ChartDataExerciseCategory,
   ChartDataExerciseCategoryBase,
   LoadedChartType,
@@ -19,15 +20,36 @@ export const UpdateChartCommentMapForExercise = (
 ): UpdateChartCommentMapForExerciseReturnType => {
   let areCommentsAlreadyLoaded = false;
   const updatedChartCommentMap = new Map(chartCommentMap);
-  let dataKey = "";
+  let existingDataKey = "";
 
   for (const option of loadExerciseOptionsMap.keys()) {
     const chartName: ChartDataExerciseCategory = `${option}_${exerciseId}`;
 
     if (loadedCharts.has(chartName)) {
       areCommentsAlreadyLoaded = true;
-      dataKey = chartName;
+      existingDataKey = chartName;
       break;
+    }
+  }
+
+  if (areCommentsAlreadyLoaded) {
+    const newDataKeys: Set<ChartDataExerciseCategory> = new Set(
+      [...loadExerciseOptions].map(
+        (option) => `${option}_${exerciseId}` as ChartDataExerciseCategory
+      )
+    );
+
+    for (const [, chartCommentList] of updatedChartCommentMap) {
+      for (const chartComment of chartCommentList) {
+        if (chartComment.dataKeys.has(existingDataKey as ChartDataCategory)) {
+          const updatedDataKeys = new Set([
+            ...chartComment.dataKeys,
+            ...newDataKeys,
+          ]);
+
+          chartComment.dataKeys = updatedDataKeys;
+        }
+      }
     }
   }
 
