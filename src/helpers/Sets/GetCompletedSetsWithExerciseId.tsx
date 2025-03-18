9634a855c717d8ef89f6b1fd05bcfeafc345d916
Viktor <vikt.mag@gmail.com>
2025-03-18 05:03:48 +0100

    Add getOnlyTimeCompleted prop to GetCompletedSetsWithExerciseId helper function

--- src/helpers/Sets/GetCompletedSetsWithExerciseId.tsx
+++ src/helpers/Sets/GetCompletedSetsWithExerciseId.tsx
@@ -1,12 +1,17 @@
 import Database from "tauri-plugin-sql-api";
 import { WorkoutSet } from "../../typings";
 
-export const GetCompletedSetsWithExerciseId = async (exerciseId: number) => {
+export const GetCompletedSetsWithExerciseId = async (
+  exerciseId: number,
+  getOnlyTimeCompleted?: boolean
+) => {
   try {
     const db = await Database.load(import.meta.env.VITE_DB);
 
+    const columns = getOnlyTimeCompleted ? "time_completed" : "*";
+
     const result = await db.select<WorkoutSet[]>(
-      `SELECT * FROM sets 
+      `SELECT ${columns} FROM sets 
        WHERE exercise_id = $1 
         AND is_template = 0
         AND is_completed = 1 
