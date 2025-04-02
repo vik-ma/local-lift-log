aa4e5db2d52e9431a26d7162dac3595fab54e1fb
Viktor <vikt.mag@gmail.com>
2025-04-03 01:47:45 +0200

    Tweak colors in DietPhaseTypeSpan

--- src/components/DietPhaseTypeSpan.tsx
+++ src/components/DietPhaseTypeSpan.tsx
@@ -4,13 +4,13 @@ type DietPhaseTypeSpanProps = {
 
 export const DietPhaseTypeSpan = ({ value }: DietPhaseTypeSpanProps) => {
   if (value === "Bulk")
-    return <span className="text-xs text-violet-600">Bulk</span>;
+    return <span className="text-xs text-[#B1C11B]">Bulk</span>;
 
   if (value === "Cut")
-    return <span className="text-xs text-indigo-400">Cut</span>;
+    return <span className="text-xs text-[#BAB095]">Cut</span>;
 
   if (value === "Maintenance")
-    return <span className="text-xs text-slate-500">Maintenance</span>;
+    return <span className="text-xs text-[#948F4E]">Maintenance</span>;
 
   return <></>;
 };
