190a85b9ad64ebab82371db80f317d17e3c24280
Viktor <vikt.mag@gmail.com>
2025-05-04 16:08:43 +0200

    Update GetLatestUserWeight helper function for body_measurements table

--- src/helpers/Measurements/GetLatestUserWeight.tsx
+++ src/helpers/Measurements/GetLatestUserWeight.tsx
@@ -1,23 +1,30 @@
 import Database from "tauri-plugin-sql-api";
-import { UserWeight } from "../../typings";
 import { FormatDateTimeString } from "..";
+
+type LatestUserWeight = {
+  weight: number;
+  weight_unit: string;
+  date: string;
+  formattedDate: string;
+};
 
 export const GetLatestUserWeight = async (clockStyle: string) => {
   try {
     const db = await Database.load(import.meta.env.VITE_DB);
 
-    // Get the user_weight row with the latest valid ISO 8601 date string value
-    const result = await db.select<UserWeight[]>(
-      `SELECT *
-       FROM user_weights
-       WHERE date IS NOT NULL 
+    // Get the body_measurements row with the latest valid ISO 8601 date string value and weight
+    const result = await db.select<LatestUserWeight[]>(
+      `SELECT weight, weight_unit, date 
+       FROM body_measurements
+       WHERE weight > 0 
+        AND date IS NOT NULL 
         AND date LIKE '____-__-__T__:__:__.___Z'
         AND date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9][0-9][0-9]Z'
        ORDER BY date DESC
        LIMIT 1`
     );
 
-    const userWeight: UserWeight = result[0];
+    const userWeight: LatestUserWeight = result[0];
 
     if (userWeight === undefined) return undefined;
 
