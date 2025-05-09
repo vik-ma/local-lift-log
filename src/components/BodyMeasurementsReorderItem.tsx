31a0ac8bce9d56b91ec1bf67a02661c9c310c14e
Viktor <vikt.mag@gmail.com>
2025-05-09 12:13:36 +0200

    Add BF% div to isBodyFatCalculationMeasurement BodyMeasurementsReorderItems

--- src/components/BodyMeasurementsReorderItem.tsx
+++ src/components/BodyMeasurementsReorderItem.tsx
@@ -36,7 +36,12 @@ export const BodyMeasurementsReorderItem = ({
       dragControls={dragControls}
       onDragEnd={() => updateActiveTrackingMeasurementOrder()}
     >
-      <div className="flex gap-1.5 items-center">
+      <div className="flex gap-1.5 items-center relative">
+        {isBodyFatCalculationMeasurement && (
+          <div className="absolute left-[14rem] top-2.5 px-2 py-1 rounded-md text-sm text-yellow-600 bg-primary/35 z-50">
+            BF%
+          </div>
+        )}
         <Input
           classNames={{
             inputWrapper: isBodyFatCalculationMeasurement
