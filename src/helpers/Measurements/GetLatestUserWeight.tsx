f35646313d97a11f955b0ce70f0d3a282e1a3811
Viktor <vikt.mag@gmail.com>
2025-05-12 19:18:38 +0200

    Make GetLatestUserWeight helper function only fetch rows with valid weight_units

--- src/helpers/Measurements/GetLatestUserWeight.tsx
+++ src/helpers/Measurements/GetLatestUserWeight.tsx
@@ -2,7 +2,6 @@ import Database from "tauri-plugin-sql-api";
 import { FormatDateTimeString } from "..";
 import { UserWeight } from "../../typings";
 
-
 export const GetLatestUserWeight = async (clockStyle: string) => {
   try {
     const db = await Database.load(import.meta.env.VITE_DB);
@@ -12,6 +11,7 @@ export const GetLatestUserWeight = async (clockStyle: string) => {
       `SELECT weight, weight_unit, date 
        FROM body_measurements
        WHERE weight > 0 
+        AND weight_unit IN ('kg', 'lbs')
         AND date IS NOT NULL 
         AND date LIKE '____-__-__T__:__:__.___Z'
         AND date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9][0-9][0-9]Z'
