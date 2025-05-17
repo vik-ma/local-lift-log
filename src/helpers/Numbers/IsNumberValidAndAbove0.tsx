0bbae8134eb9519e843893a7aff2e7784931f288
Viktor <vikt.mag@gmail.com>
2025-05-17 23:00:17 +0200

    Delete IsNumberValidAndAbove0 helper function

--- src/helpers/Numbers/IsNumberValidAndAbove0.tsx
+++
@@ -1,6 +0,0 @@
-export const IsNumberValidAndAbove0 = (number: number): boolean => {
-  if (isNaN(number)) return false;
-  if (number <= 0) return false;
-  if (!isFinite(number)) return false;
-  return true;
-};
