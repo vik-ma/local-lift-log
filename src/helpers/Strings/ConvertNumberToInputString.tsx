06dbe2b6164c5c4ad0ec4369e20369317fa5e6d5
Viktor <vikt.mag@gmail.com>
2025-05-17 23:08:03 +0200

    Refactor ConvertNumberToInputString helper function with new props

--- src/helpers/Strings/ConvertNumberToInputString.tsx
+++ src/helpers/Strings/ConvertNumberToInputString.tsx
@@ -1,8 +1,20 @@
-import { IsNumberValid } from "..";
+import { IsNumberValid, IsNumberValidInteger } from "..";
 
-export const ConvertNumberToInputString = (num: number, allow0?: boolean) => {
-  if (allow0 && IsNumberValid(num)) return num.toString();
-  if (!allow0 && IsNumberValid(num, 0, true)) return num.toString();
+export const ConvertNumberToInputString = (
+  num: number,
+  minValue: number = 0,
+  doNotAllowMinValue?: boolean,
+  maxValue?: number,
+  isInteger?: boolean
+) => {
+  if (
+    isInteger &&
+    IsNumberValidInteger(num, minValue, doNotAllowMinValue, maxValue)
+  )
+    return num.toString();
+
+  if (!isInteger && IsNumberValid(num, minValue, doNotAllowMinValue, maxValue))
+    return num.toString();
 
   return "";
 };
