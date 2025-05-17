b27ec2169a8bac9f4ac7ba4165144ee90f39ad47
Viktor <vikt.mag@gmail.com>
2025-05-17 23:02:59 +0200

    Add doNotAllowMinValue and maxValue props to IsNumberValidInteger helper function

--- src/helpers/Numbers/IsNumberValidInteger.tsx
+++ src/helpers/Numbers/IsNumberValidInteger.tsx
@@ -1,6 +1,13 @@
-export const IsNumberValidInteger = (number: number, minValue = 0) => {
+export const IsNumberValidInteger = (
+  number: number,
+  minValue: number = 0,
+  doNotAllowMinValue?: boolean,
+  maxValue?: number
+) => {
   if (isNaN(number)) return false;
+  if (doNotAllowMinValue && number <= minValue) return false;
   if (number < minValue) return false;
+  if (maxValue !== undefined && number > maxValue) return false;
   if (!Number.isInteger(number)) return false;
   return true;
 };
