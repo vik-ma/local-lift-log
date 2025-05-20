e82c77189e8546b08730a90b967b59cb6434bcda
Viktor <vikt.mag@gmail.com>
2025-05-20 20:18:41 +0200

    Refactor GetValidatedSetValue helper function

--- src/helpers/Sets/GetValidatedSetValue.tsx
+++ src/helpers/Sets/GetValidatedSetValue.tsx
@@ -1,24 +1,31 @@
-import { IsNumberValid } from "..";
+import { IsNumberValid, IsNumberValidInteger } from "..";
 
-export const GetValidatedSetValue = (
-  value: number,
-  isRir?: boolean,
-  isRpe?: boolean
-) => {
-  if (isRir) {
-    if (isNaN(value)) return -1;
-    if (value < -1) return -1;
-    if (!isFinite(value)) return -1;
-    return value;
+export const GetValidatedSetValue = (value: number, key: string) => {
+  switch (key) {
+    case "weight":
+      return IsNumberValid(value) ? value : 0;
+    case "reps": {
+      return IsNumberValidInteger(value) ? value : 0;
+    }
+    case "distance": {
+      return IsNumberValid(value) ? value : 0;
+    }
+    case "time": {
+      return IsNumberValidInteger(value) ? value : 0;
+    }
+    case "rir": {
+      return IsNumberValidInteger(value, -1) ? value : -1;
+    }
+    case "rpe": {
+      return IsNumberValidInteger(value, 0, false, 10) ? value : 0;
+    }
+    case "resistance_level": {
+      return IsNumberValid(value) ? value : 0;
+    }
+    case "partial_reps": {
+      return IsNumberValidInteger(value) ? value : 0;
+    }
+    default:
+      return 0;
   }
-
-  if (isRpe) {
-    if (isNaN(value)) return -1;
-    if (value < 1 || value > 10) return -1;
-    return value;
-  }
-
-  if (IsNumberValid(value)) return value;
-
-  return 0;
 };
