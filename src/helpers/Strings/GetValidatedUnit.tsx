cf84ad9dde16970bd967d9fda98968e5ce2c2e22
Viktor <vikt.mag@gmail.com>
2025-06-04 20:25:21 +0200

    Add distance, caliper and circumference validation to GetValidatedUnit helper function

--- src/helpers/Strings/GetValidatedUnit.tsx
+++ src/helpers/Strings/GetValidatedUnit.tsx
@@ -1,12 +1,32 @@
-import { ValidWeightUnits } from "..";
+import {
+  ValidDistanceUnits,
+  ValidMeasurementUnits,
+  ValidWeightUnits,
+} from "..";
 
-type UnitType = "weight";
+type UnitType = "weight" | "distance" | "caliper" | "circumference";
 
 export const GetValidatedUnit = (unit: string, unitType: UnitType) => {
   if (unitType === "weight") {
     if (ValidWeightUnits().includes(unit)) return unit;
 
     return ValidWeightUnits()[0];
+  }
+
+  if (unitType === "distance") {
+    if (ValidDistanceUnits().includes(unit)) return unit;
+
+    return ValidDistanceUnits()[0];
+  }
+
+  if (unitType === "caliper") {
+    return "mm";
+  }
+
+  if (unitType === "circumference") {
+    if (ValidMeasurementUnits().includes(unit)) return unit;
+
+    return ValidMeasurementUnits()[0];
   }
 
   return "";
