2d6a9687378f6d0a85106f5677b788d09b427f18
Viktor <vikt.mag@gmail.com>
2025-06-23 20:52:10 +0200

    Add Set Note to SetNotesModal

--- src/components/Modals/SetNotesModal.tsx
+++ src/components/Modals/SetNotesModal.tsx
@@ -37,7 +37,20 @@ export const SetNotesModal = ({
           <>
             <ModalHeader>{operatingSet.exercise_name} Set Notes</ModalHeader>
             <ModalBody>
-              <ScrollShadow className="h-[400px]"></ScrollShadow>
+              <ScrollShadow className="flex flex-col h-[400px]">
+                <div className="flex flex-col gap-px">
+                  <h3 className="text-lg font-semibold">Set Note</h3>
+                  {operatingSet.note === null ? (
+                    <span className="text-stone-400 italic text-sm">
+                      No Set Note
+                    </span>
+                  ) : (
+                    <span className="text-stone-500 text-sm">
+                      {operatingSet.note}
+                    </span>
+                  )}
+                </div>
+              </ScrollShadow>
             </ModalBody>
             <ModalFooter>
               <Button color="primary" variant="light" onPress={onClose}>
