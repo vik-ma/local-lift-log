1ce1779ecb75895e3cbc3cfe0015e14510d0fbb4
Viktor <vikt.mag@gmail.com>
2025-06-28 16:54:15 +0200

    Add hasNote and hasComment props to CommentIcon component to alter its color

--- src/assets/CommentIcon.tsx
+++ src/assets/CommentIcon.tsx
@@ -1,8 +1,37 @@
+import { useId } from "react";
+
 type CommentIconProps = {
   size?: number;
+  hasNote?: boolean;
+  hasComment?: boolean;
 };
 
-export const CommentIcon = ({ size }: CommentIconProps) => {
+export const CommentIcon = ({
+  size,
+  hasNote,
+  hasComment,
+}: CommentIconProps) => {
+  // LinearGradient Id does not work if multiple CommentIcons are being rendered on the page
+  const gradientId = useId();
+
+  const hasNoteAndComment = hasNote && hasComment;
+
+  const color1 = hasNoteAndComment
+    ? "#818cf8"
+    : hasComment
+    ? "#d7a200"
+    : hasNote
+    ? "#818cf8"
+    : "#808080";
+
+  const color2 = hasNoteAndComment
+    ? "#d7a200"
+    : hasComment
+    ? "#d7a200"
+    : hasNote
+    ? "#818cf8"
+    : "#808080";
+
   return (
     <svg
       width={size || 20}
@@ -11,19 +40,13 @@ export const CommentIcon = ({ size }: CommentIconProps) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <defs>
-        <linearGradient
-          id="diagonalGradient"
-          x1="0%"
-          y1="0%"
-          x2="100%"
-          y2="100%"
-        >
-          <stop offset="0%" stopColor="#808080" />
-          <stop offset="100%" stopColor="#808080" />
+        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor={color1} />
+          <stop offset="100%" stopColor={color2} />
         </linearGradient>
       </defs>
       <path
-        fill="url(#diagonalGradient)"
+        fill={`url(#${gradientId})`}
         fillRule="evenodd"
         d="M12.0867962,18 L6,21.8042476 L6,18 L4,18 C2.8954305,18 2,17.1045695 2,16 L2,4 C2,2.8954305 2.8954305,2 4,2 L20,2 C21.1045695,2 22,2.8954305 22,4 L22,16 C22,17.1045695 21.1045695,18 20,18 L12.0867962,18 Z M8,18.1957524 L11.5132038,16 L20,16 L20,4 L4,4 L4,16 L8,16 L8,18.1957524 Z M7,13 L7,11 L14,11 L14,13 L7,13 Z M7,9 L7,7 L16,7 L16,9 L7,9 Z"
       />
