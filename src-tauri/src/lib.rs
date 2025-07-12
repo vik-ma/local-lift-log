d9b04ceb8ac45c5cc8608bd779192f186c19d7a2
Viktor <vikt.mag@gmail.com>
2025-07-12 13:10:27 +0200

    Add lib.rs and modify main.rs for Tauri v2

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,22 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod db;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|_app| {
+            db::init();
+            Ok(())
+        })
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
