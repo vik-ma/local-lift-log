323993aed700b3915f1d6a854e55e5f4fe6a0749
Viktor <vikt.mag@gmail.com>
2025-07-13 15:42:50 +0200

    Install Tauri Store plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|_app| {
