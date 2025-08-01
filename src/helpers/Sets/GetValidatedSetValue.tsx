1244da6e33acfce108edbf030fc0b9a790241969
Viktor <vikt.mag@gmail.com>
2025-08-01 19:45:49 +0200

    Add named variables to GetValidatedSetValue helper function

--- src/helpers/Sets/GetValidatedSetValue.tsx
+++ src/helpers/Sets/GetValidatedSetValue.tsx
@@ -14,27 +14,28 @@ export const GetValidatedSetValue = (value: number, key: SetTrackingKeys) => {
   switch (key) {
     case "weight":
       return IsNumberValid(value) ? value : 0;
-    case "reps": {
+    case "reps":
       return IsNumberValidInteger(value) ? value : 0;
-    }
-    case "distance": {
+    case "distance":
       return IsNumberValid(value) ? value : 0;
-    }
-    case "time": {
+    case "time":
       return IsNumberValidInteger(value) ? value : 0;
-    }
     case "rir": {
-      return IsNumberValidInteger(value, -1) ? value : -1;
+      const minValue = -1;
+      return IsNumberValidInteger(value, minValue) ? value : -1;
     }
     case "rpe": {
-      return IsNumberValidInteger(value, 0, false, 10) ? value : 0;
+      const minValue = 0;
+      const doNotAllowMinValue = false;
+      const maxValue = 10;
+      return IsNumberValidInteger(value, minValue, doNotAllowMinValue, maxValue)
+        ? value
+        : 0;
     }
-    case "resistance_level": {
+    case "resistance_level":
       return IsNumberValid(value) ? value : 0;
-    }
-    case "partial_reps": {
+    case "partial_reps":
       return IsNumberValidInteger(value) ? value : 0;
-    }
     default:
       return 0;
   }
