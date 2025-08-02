dbd0c11e015d0d1fe9a32df2ffdbe823803a73aa
Viktor <vikt.mag@gmail.com>
2025-08-02 20:03:54 +0200

    Remove locale validation in ConvertCalendarDateToLocalizedString

--- src/helpers/Dates/ConvertCalendarDateToLocalizedString.tsx
+++ src/helpers/Dates/ConvertCalendarDateToLocalizedString.tsx
@@ -4,15 +4,12 @@ export const ConvertCalendarDateToLocalizedString = (
   calendarDate: CalendarDate,
   locale: string
 ) => {
-  const localeString =
-    locale === "en-GB" || locale === "en-US" ? locale : "en-GB";
-
   const date = new Date(calendarDate.toString());
 
   try {
     const dateString = parseAbsoluteToLocal(date.toISOString())
       .toDate()
-      .toLocaleDateString(localeString);
+      .toLocaleDateString(locale);
     return dateString;
   } catch {
     return "Invalid DateTime";
