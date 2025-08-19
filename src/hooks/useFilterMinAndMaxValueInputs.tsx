62ace7e6a6f46d369e94ea401598f64810b77217
Viktor <vikt.mag@gmail.com>
2025-08-19 15:40:26 +0200

    Add min and doNotAllowMin variables to useFilterMinAndMaxValueInputs hook

--- src/hooks/useFilterMinAndMaxValueInputs.tsx
+++ src/hooks/useFilterMinAndMaxValueInputs.tsx
@@ -19,23 +19,28 @@ export const useFilterMinAndMaxValueInputs = ({
   const [includeNullInMaxValues, setIncludeNullInMaxValues] =
     useState<boolean>(false);
 
+  const min = 0;
+  const doNotAllowMin = true;
+
   const isMinInputInvalid = useMemo(() => {
     if (IsStringEmpty(minInput)) return false;
-    if (isIntegerOnly && IsStringInvalidInteger(minInput, 0, true)) return true;
-    if (IsStringInvalidNumber(minInput, 0, true)) return true;
+    if (isIntegerOnly && IsStringInvalidInteger(minInput, min, doNotAllowMin))
+      return true;
+    if (IsStringInvalidNumber(minInput, min, doNotAllowMin)) return true;
     if (minValue !== undefined && Number(minInput) < minValue) return true;
     if (maxValue !== undefined && Number(minInput) > maxValue) return true;
     return false;
-  }, [minInput, minValue, maxValue, isIntegerOnly]);
+  }, [minInput, minValue, maxValue, isIntegerOnly, doNotAllowMin]);
 
   const isMaxInputInvalid = useMemo(() => {
     if (IsStringEmpty(maxInput)) return false;
-    if (isIntegerOnly && IsStringInvalidInteger(maxInput, 0, true)) return true;
-    if (IsStringInvalidNumber(maxInput, 0, true)) return true;
+    if (isIntegerOnly && IsStringInvalidInteger(maxInput, min, doNotAllowMin))
+      return true;
+    if (IsStringInvalidNumber(maxInput, min, doNotAllowMin)) return true;
     if (minValue !== undefined && Number(maxInput) < minValue) return true;
     if (maxValue !== undefined && Number(maxInput) > maxValue) return true;
     return false;
-  }, [maxInput, minValue, maxValue, isIntegerOnly]);
+  }, [maxInput, minValue, maxValue, isIntegerOnly, doNotAllowMin]);
 
   const isMaxValueBelowMinValue = useMemo(() => {
     if (isMinInputInvalid || isMaxInputInvalid) return false;
