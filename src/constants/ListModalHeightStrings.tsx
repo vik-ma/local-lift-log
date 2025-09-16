4d2d0468ce891210269b3021d9ac100394f74744
Viktor <vikt.mag@gmail.com>
2025-09-16 17:28:37 +0200

    Add TAB_LIST_HEIGHT_WITHOUT_PAGINATION and TAB_LIST_HEIGHT_WITH_PAGINATION consts

--- src/constants/ListModalHeightStrings.tsx
+++ src/constants/ListModalHeightStrings.tsx
@@ -2,3 +2,5 @@ export const MODAL_BODY_HEIGHT = "h-[450px]";
 export const LIST_HEIGHT_WITH_PAGINATION = "h-[316px]";
 export const LIST_HEIGHT_WITHOUT_PAGINATION = "h-[354px]";
 export const MODAL_TAB_HEIGHT = "h-[408px]";
+export const TAB_LIST_HEIGHT_WITH_PAGINATION = "h-[274px]";
+export const TAB_LIST_HEIGHT_WITHOUT_PAGINATION = "h-[312px]";
