d0302ecc757a677622c039ec2e1df94d8e6a3204
Viktor <vikt.mag@gmail.com>
2025-09-19 19:25:22 +0200

    Add Pagination controls to WorkoutTemplateModalList

--- src/components/ModalLists/WorkoutTemplateModalList.tsx
+++ src/components/ModalLists/WorkoutTemplateModalList.tsx
@@ -1,4 +1,4 @@
-import { Button, ScrollShadow } from "@heroui/react";
+import { Button, Pagination, ScrollShadow } from "@heroui/react";
 import {
   UseWorkoutTemplateListReturnType,
   WorkoutTemplate,
@@ -13,7 +13,11 @@ import {
 } from "..";
 import { GoToArrowIcon } from "../../assets";
 import { FormatNumExercisesAndSetsStrings } from "../../helpers";
-import { MODAL_BODY_HEIGHT } from "../../constants";
+import {
+  LIST_HEIGHT_WITH_PAGINATION,
+  LIST_HEIGHT_WITHOUT_PAGINATION,
+  MODAL_BODY_HEIGHT,
+} from "../../constants";
 
 type WorkoutTemplateModalListProps = {
   useWorkoutTemplateList: UseWorkoutTemplateListReturnType;
@@ -33,11 +37,21 @@ export const WorkoutTemplateModalList = ({
     workoutTemplates,
     listFilters,
     isWorkoutTemplateListLoaded,
+    paginatedWorkoutTemplates,
+    totalPaginationPages,
+    validPaginationPage,
+    setPaginationPage,
   } = useWorkoutTemplateList;
 
   const { filterMap, removeFilter, prefixMap } = listFilters;
 
   const navigate = useNavigate();
+
+  const showPaginationControls = totalPaginationPages > 1;
+
+  const listHeight = showPaginationControls
+    ? LIST_HEIGHT_WITH_PAGINATION
+    : LIST_HEIGHT_WITHOUT_PAGINATION;
 
   return (
     <div className={`${MODAL_BODY_HEIGHT} flex flex-col gap-1.5`}>
@@ -73,41 +87,58 @@ export const WorkoutTemplateModalList = ({
         )}
       </div>
       {isWorkoutTemplateListLoaded.current ? (
-        <ScrollShadow className="flex flex-col gap-1">
-          {filteredWorkoutTemplates.map((template) => (
-            <button
-              className={
-                filterWorkoutTemplates?.has(template.id)
-                  ? "flex flex-col justify-start items-start bg-amber-100 border-2 border-amber-300 rounded-xl px-2 py-1 hover:border-default-400 focus:border-default-400"
-                  : "flex flex-col justify-start items-start bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:border-default-400 focus:border-default-400"
-              }
-              key={template.id}
-              onClick={() => onClickAction(template)}
-            >
-              <span className="w-full truncate text-left">{template.name}</span>
-              {template.numSets! > 0 ? (
-                <span className="text-xs text-secondary text-left">
-                  {FormatNumExercisesAndSetsStrings(
-                    template.exerciseIdSet !== undefined
-                      ? template.exerciseIdSet.size
-                      : 0,
-                    template.numSets
-                  )}
+        <div className="flex flex-col justify-between gap-1.5">
+          <ScrollShadow className={`${listHeight} flex flex-col gap-1`}>
+            {paginatedWorkoutTemplates.map((template) => (
+              <button
+                className={
+                  filterWorkoutTemplates?.has(template.id)
+                    ? "flex flex-col justify-start items-start bg-amber-100 border-2 border-amber-300 rounded-xl px-2 py-1 hover:border-default-400 focus:border-default-400"
+                    : "flex flex-col justify-start items-start bg-default-100 border-2 border-default-200 rounded-xl px-2 py-1 hover:border-default-400 focus:border-default-400"
+                }
+                key={template.id}
+                onClick={() => onClickAction(template)}
+              >
+                <span className="w-full truncate text-left">
+                  {template.name}
                 </span>
-              ) : (
-                <span className="text-xs text-stone-400 text-left">Empty</span>
-              )}
-              {template.note !== null && (
-                <span className="w-full break-all text-xs text-stone-500 text-left">
-                  {template.note}
-                </span>
-              )}
-            </button>
-          ))}
-          {filteredWorkoutTemplates.length === 0 && (
-            <EmptyListLabel itemName="Workout Templates" />
+                {template.numSets! > 0 ? (
+                  <span className="text-xs text-secondary text-left">
+                    {FormatNumExercisesAndSetsStrings(
+                      template.exerciseIdSet !== undefined
+                        ? template.exerciseIdSet.size
+                        : 0,
+                      template.numSets
+                    )}
+                  </span>
+                ) : (
+                  <span className="text-xs text-stone-400 text-left">
+                    Empty
+                  </span>
+                )}
+                {template.note !== null && (
+                  <span className="w-full break-all text-xs text-stone-500 text-left">
+                    {template.note}
+                  </span>
+                )}
+              </button>
+            ))}
+            {filteredWorkoutTemplates.length === 0 && (
+              <EmptyListLabel itemName="Workout Templates" />
+            )}
+          </ScrollShadow>
+          {showPaginationControls && (
+            <div className="flex justify-center">
+              <Pagination
+                showControls
+                loop
+                page={validPaginationPage}
+                total={totalPaginationPages}
+                onChange={setPaginationPage}
+              />
+            </div>
           )}
-        </ScrollShadow>
+        </div>
       ) : (
         <LoadingSpinner />
       )}
