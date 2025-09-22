5d1e10d6590719a7a64c19d765d819fe8eff29f9
Viktor <vikt.mag@gmail.com>
2025-09-22 19:50:02 +0200

    Add height and Calendar to CalendarModal

--- src/components/Modals/CalendarModal.tsx
+++ src/components/Modals/CalendarModal.tsx
@@ -5,8 +5,10 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
+  Calendar,
 } from "@heroui/react";
 import { UseDisclosureReturnType, UserSettings } from "../../typings";
+import { MODAL_BODY_HEIGHT } from "../../constants";
 
 type CalendarModalProps = {
   calendarModal: UseDisclosureReturnType;
@@ -17,7 +19,6 @@ export const CalendarModal = ({
   calendarModal,
   userSettings,
 }: CalendarModalProps) => {
-
   return (
     <Modal
       isOpen={calendarModal.isOpen}
@@ -27,7 +28,13 @@ export const CalendarModal = ({
         {(onClose) => (
           <>
             <ModalHeader>Calendar</ModalHeader>
-            <ModalBody className="py-0"></ModalBody>
+            <ModalBody className="py-0">
+              <div
+                className={`${MODAL_BODY_HEIGHT} flex flex-col items-center gap-1.5`}
+              >
+                <Calendar calendarWidth={300} showMonthAndYearPickers />
+              </div>
+            </ModalBody>
             <ModalFooter>
               <Button color="primary" variant="light" onPress={onClose}>
                 Close
