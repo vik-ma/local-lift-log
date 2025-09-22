2f7a5169b1de4cdf01edd9d6e4321b9fd7aedc09
Viktor <vikt.mag@gmail.com>
2025-09-22 20:11:24 +0200

    Add useEffect to highlight date in CalendarModal

--- src/components/Modals/CalendarModal.tsx
+++ src/components/Modals/CalendarModal.tsx
@@ -9,6 +9,7 @@ import {
 } from "@heroui/react";
 import { UseDisclosureReturnType, UserSettings } from "../../typings";
 import { MODAL_BODY_HEIGHT } from "../../constants";
+import { useEffect } from "react";
 
 type CalendarModalProps = {
   calendarModal: UseDisclosureReturnType;
@@ -19,6 +20,28 @@ export const CalendarModal = ({
   calendarModal,
   userSettings,
 }: CalendarModalProps) => {
+  useEffect(() => {
+    const el = document.querySelector(
+      '[aria-label="Monday, September 8, 2025"]'
+    ) as HTMLElement;
+
+    if (el) {
+      const overlay = document.createElement("div");
+
+      overlay.style.position = "absolute";
+      overlay.style.top = "0";
+      overlay.style.left = "0";
+      overlay.style.width = "100%";
+      overlay.style.height = "100%";
+      overlay.style.backgroundColor = "rgba(0,0,0,0.2)";
+      overlay.style.pointerEvents = "none"; 
+
+      el.style.position = "relative"; 
+
+      el.appendChild(overlay);
+    }
+  }, [calendarModal.isOpen]);
+
   return (
     <Modal
       isOpen={calendarModal.isOpen}
@@ -32,7 +55,7 @@ export const CalendarModal = ({
               <div
                 className={`${MODAL_BODY_HEIGHT} flex flex-col items-center gap-1.5`}
               >
-                <Calendar calendarWidth={300} showMonthAndYearPickers />
+                <Calendar calendarWidth={280} showMonthAndYearPickers />
               </div>
             </ModalBody>
             <ModalFooter>
