3a0a60bc7824ef1c9288946d2a18c0090eef16dc
Viktor <vikt.mag@gmail.com>
2025-09-29 01:45:56 +0200

    Change SQL query in GetCalendarWorkoutList to also get a list of all exercise_group_set_string_primary for completed sets in workout

--- src/helpers/Workouts/GetCalendarWorkoutList.tsx
+++ src/helpers/Workouts/GetCalendarWorkoutList.tsx
@@ -7,18 +7,26 @@ export const GetCalendarWorkoutList = async (yearMonthString: string) => {
 
     const result = await db.select<CalendarWorkoutItem[]>(
       `SELECT 
-        w.id,
-        w.date,
-        w.workout_template_id,
-        CASE 
-          WHEN w.workout_template_id = 0 THEN 'No Workout Template'
-          WHEN t.id IS NULL THEN 'Unknown Workout Template'
-          ELSE t.name
-        END AS workout_template_name
+          w.id,
+          w.date,
+          w.workout_template_id,
+          CASE 
+              WHEN w.workout_template_id = 0 THEN 'No Workout Template'
+              WHEN t.id IS NULL THEN 'Unknown Workout Template'
+              ELSE t.name
+          END AS workout_template_name,
+          COALESCE(GROUP_CONCAT(DISTINCT e.exercise_group_set_string_primary), '') 
+              AS exercise_groups_string
        FROM workouts w
        LEFT JOIN workout_templates t 
-        ON w.workout_template_id = t.id
-       WHERE date LIKE '${yearMonthString}%';`
+          ON w.workout_template_id = t.id
+       LEFT JOIN sets s 
+          ON w.id = s.workout_id AND s.is_completed = 1
+       LEFT JOIN exercises e 
+           ON s.exercise_id = e.id
+       WHERE w.date LIKE '${yearMonthString}%'
+       GROUP BY w.id
+       ORDER BY w.date`
     );
 
     return result;
