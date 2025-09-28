6982b90e429d5b0d3732e93ba92c93783145db2c
Viktor <vikt.mag@gmail.com>
2025-09-28 21:03:21 +0200

    Rename workouts-templates key in CalendarDateMarkingsMap to workout-templates

--- src/constants/CalendarDateMarkingsMap.tsx
+++ src/constants/CalendarDateMarkingsMap.tsx
@@ -1,7 +1,7 @@
 export const CALENDAR_DATE_MARKINGS_MAP = Object.freeze(
   new Map([
     ["workouts", "Workouts"],
-    ["workouts-templates", "Workout Templates"],
+    ["workout-templates", "Workout Templates"],
     ["exercise-groups", "Exercise Groups"],
     ["active-routine", "Active Routine"],
     ["none", "None"],
