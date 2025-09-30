1cc7386edc14dd83aff82ce086e1ccfae2ec995e
Viktor <vikt.mag@gmail.com>
2025-09-30 04:12:00 +0200

    Change order of colors in CalendarColorList

--- src/constants/CalendarColorList.tsx
+++ src/constants/CalendarColorList.tsx
@@ -1,14 +1,14 @@
 export const CALENDAR_COLOR_LIST = [
   // Alpha value of 70% (B3)
   "#007FFFB3", // Blue
-  "#FFA500B3", // Orange/Yellow
-  "#00FF00B3", // Green
   "#FF2C2CB3", // Red
+  "#00FF00B3", // Green
+  "#FFA500B3", // Orange/Yellow
   "#00F0FFB3", // Cyan
   "#6400CCB3", // Violet
   "#898989B3", // Gray
-  "#80521BB3", // Brown
   "#FF1DCEB3", // Magenta
+  "#80521BB3", // Brown
   "#6D8196B3", // Blue/Gray
   "#FFCC33B3", // Yellow
   "#111184B3", // Dark Blue
