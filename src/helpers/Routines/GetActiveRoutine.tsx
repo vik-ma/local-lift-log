6e1ed1149d9b4ea49d4a647e8a57ee37d3b90375
Viktor <vikt.mag@gmail.com>
2025-10-02 11:18:11 +0200

    Change SQL-query in GetActiveRoutine to also fetch workout_routine_schedules

--- src/helpers/Routines/GetActiveRoutine.tsx
+++ src/helpers/Routines/GetActiveRoutine.tsx
@@ -6,7 +6,25 @@ export const GetActiveRoutine = async (activeRoutineId: number) => {
     const db = await Database.load(import.meta.env.VITE_DB);
 
     const result = await db.select<Routine[]>(
-      `SELECT * FROM routines WHERE id = $1`,
+      `SELECT 
+        routines.*,
+        json_group_array(
+          CASE 
+            WHEN workout_routine_schedules.workout_template_id IS NULL 
+            THEN NULL
+            ELSE json_object(
+              'workout_template_id', workout_routine_schedules.workout_template_id,
+              'day', workout_routine_schedules.day,
+              'name', workout_templates.name
+            )
+          END
+        ) AS routineSchedules
+       FROM routines
+       LEFT JOIN workout_routine_schedules 
+         ON routines.id = workout_routine_schedules.routine_id
+       LEFT JOIN workout_templates
+         ON workout_routine_schedules.workout_template_id = workout_templates.id
+       WHERE routines.id = $1`,
       [activeRoutineId]
     );
 
