daf1ff2ae349935506b5802dd4e0fe07802b7d9d
Viktor <vikt.mag@gmail.com>
2025-10-08 19:05:13 +0200

    Add optional getOnlyCompletedSets prop to GetWorkoutSetList helper function

--- src/helpers/Workouts/GetWorkoutSetList.tsx
+++ src/helpers/Workouts/GetWorkoutSetList.tsx
@@ -2,9 +2,14 @@ import Database from "@tauri-apps/plugin-sql";
 import { WorkoutSet } from "../../typings";
 
 export const GetWorkoutSetList = async (
-  workoutId: number
+  workoutId: number,
+  getOnlyCompletedSets?: boolean
 ): Promise<WorkoutSet[]> => {
   try {
+    const isCompletedConditional = getOnlyCompletedSets
+      ? "AND is_completed = 1"
+      : "";
+
     const db = await Database.load(import.meta.env.VITE_DB);
 
     const result = await db.select<WorkoutSet[]>(
@@ -12,7 +17,7 @@ export const GetWorkoutSetList = async (
       COALESCE(exercises.name, 'Unknown Exercise') AS exercise_name
       FROM sets LEFT JOIN 
       exercises ON sets.exercise_id = exercises.id 
-      WHERE workout_id = $1 AND is_template = 0`,
+      WHERE workout_id = $1 AND is_template = 0 ${isCompletedConditional}`,
       [workoutId]
     );
 
